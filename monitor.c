f869ae8111ef39d9650680b6abbdd90740a8e582
Patrick Sanan <patrick.sanan@gmail.com>
2016-08-01 12:10:10 +0200

    PETSc Evolution: change monitor output to be valid m-files

--- monitor.c
+++ monitor.c
@@ -9,8 +9,8 @@ PetscErrorCode TSCustomMonitor(TS ts, PetscInt step, PetscReal time, Vec x, void
 
   PetscFunctionBeginUser;
 
-#if (defined DEBUGOUTPUT)
-    ierr = PetscPrintf(PETSC_COMM_WORLD,"Custom monitor called. Step %D, t=%f\n",step,time);CHKERRQ(ierr);
+#if (defined VERBOSE)
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"Custom monitor: Step %D, t=%f\n",step,time);CHKERRQ(ierr);
 #endif
 
   /* Dump the solution to a file named for the timestep */
@@ -19,7 +19,7 @@ PetscErrorCode TSCustomMonitor(TS ts, PetscInt step, PetscReal time, Vec x, void
     char filename[PETSC_MAX_PATH_LEN],vecname[PETSC_MAX_PATH_LEN];
 
 
-    ierr = PetscSNPrintf(filename,PETSC_MAX_PATH_LEN,"output/TIMESTEPPER/S_s/S_s.%D",step);CHKERRQ(ierr);
+    ierr = PetscSNPrintf(filename,PETSC_MAX_PATH_LEN,"output/TIMESTEPPER/S_s/S_s_%D.m",step);CHKERRQ(ierr);
     ierr = PetscSNPrintf(vecname,PETSC_MAX_PATH_LEN,"S_s_step_%D",step);CHKERRQ(ierr);
 
     ierr = PetscViewerASCIIOpen(PETSC_COMM_WORLD,filename,&viewer);CHKERRQ(ierr);
@@ -33,7 +33,7 @@ PetscErrorCode TSCustomMonitor(TS ts, PetscInt step, PetscReal time, Vec x, void
   {
     PetscViewer viewer;
     char filename[PETSC_MAX_PATH_LEN],vecname[PETSC_MAX_PATH_LEN];
-    ierr = PetscSNPrintf(filename,PETSC_MAX_PATH_LEN,"output/TIMESTEPPER/rhs/rhs.%D",step);CHKERRQ(ierr);
+    ierr = PetscSNPrintf(filename,PETSC_MAX_PATH_LEN,"output/TIMESTEPPER/rhs/rhs_%D.m",step);CHKERRQ(ierr);
     ierr = PetscSNPrintf(vecname,PETSC_MAX_PATH_LEN,"rhs_step_%D",step);CHKERRQ(ierr);
 
     ierr = PetscViewerASCIIOpen(PETSC_COMM_WORLD,filename,&viewer);CHKERRQ(ierr);
