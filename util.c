1b55630b107e7a128cf6105e35b1aa910b9fea59
Dan J. Bower <djbower@gmail.com>
2016-11-22 16:11:48 +0100

    make linear d/dr work again

--- util.c
+++ util.c
@@ -26,6 +26,99 @@ PetscScalar tanh_weight( PetscScalar qty, PetscScalar threshold, PetscScalar wid
 
 PetscErrorCode set_d_dr( Ctx *E )
 {
+
+    PetscFunctionBeginUser;
+
+    set_d_dr_linear( E );
+    //set_d_dr_quadratic( E );
+
+    PetscFunctionReturn(0);
+
+}
+
+
+PetscErrorCode set_d_dr_linear( Ctx *E )
+{
+
+    PetscErrorCode ierr;
+    PetscInt i, rstart, rend, col[2];
+    PetscInt numpts_s, ilo_s, ihi_s, w_s;
+    PetscInt numpts_b, ilo_b, w_b; // ihi_b, w_b;
+    PetscScalar value[2], dh, dh2;
+    PetscScalar *arr_radius_s, *arr_radius_b;
+    Mat A, B;
+    Mesh *M;
+
+    PetscFunctionBeginUser;
+
+    M = &E->mesh;
+
+    // staggered
+    ierr = DMDAGetCorners(E->da_s,&ilo_s,0,0,&w_s,0,0);CHKERRQ(ierr);
+    ihi_s = ilo_s + w_s;
+    ierr = DMDAGetInfo(E->da_s,NULL,&numpts_s,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);CHKERRQ(ierr);
+    ierr = DMDAVecGetArrayRead(E->da_s,M->radius_s,&arr_radius_s);CHKERRQ(ierr);
+
+    // basic
+    ierr = DMDAGetCorners(E->da_b,&ilo_b,0,0,&w_b,0,0);CHKERRQ(ierr);
+    //ihi_b = ilo_b + w_b;
+    ierr = DMDAGetInfo(E->da_b,NULL,&numpts_b,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);CHKERRQ(ierr);
+    ierr = DMDAVecGetArrayRead(E->da_b,M->radius_b,&arr_radius_b);CHKERRQ(ierr);
+
+    /* the first and last row are all zeros (implicitly, I think),
+       to enable us to return a vector with length numpts_b */
+
+    // initialise A coefficient matrix
+    ierr = MatCreate( PETSC_COMM_WORLD, &A );CHKERRQ(ierr);
+    ierr = MatSetSizes(A,PETSC_DECIDE,PETSC_DECIDE,numpts_b,numpts_s);CHKERRQ(ierr);
+    ierr = MatSetFromOptions(A);CHKERRQ(ierr);
+    ierr = MatSetUp(A);CHKERRQ(ierr);
+
+    // initialise B coefficient matrix
+    ierr = MatCreate( PETSC_COMM_WORLD, &B );CHKERRQ(ierr);
+    ierr = MatSetSizes(B,PETSC_DECIDE,PETSC_DECIDE,numpts_b,numpts_s);CHKERRQ(ierr);
+    ierr = MatSetFromOptions(B);CHKERRQ(ierr);
+    ierr = MatSetUp(B);CHKERRQ(ierr);
+
+    // add coefficients to matrices
+
+    /* set values corresponding to the mesh interior */
+    rstart = (ilo_s == 0      )  ? 1          : ilo_s;
+    rend   = (ihi_s == numpts_s) ? numpts_s-1 : ihi_s;
+    for (i=rstart; i<=rend; i++) {
+        col[0]=i-1; col[1]=i;
+        dh = arr_radius_s[i] - arr_radius_s[i-1];
+        // A
+        value[0] = -1.0 / dh;
+        value[1] = 1.0 / dh;
+        ierr = MatSetValues(A,1,&i,2,col,value,INSERT_VALUES); CHKERRQ(ierr);
+        // B
+        dh2 = arr_radius_b[i] - arr_radius_s[i-1];
+        value[0] = 1.0 - dh2/dh;
+        value[1] = dh2/dh;
+        ierr = MatSetValues(B,1,&i,2,col,value,INSERT_VALUES); CHKERRQ(ierr);
+    }
+
+    ierr = MatAssemblyBegin(A, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+    ierr = MatAssemblyEnd(A, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+    ierr = MatAssemblyBegin(B, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+    ierr = MatAssemblyEnd(B, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+
+    /* store to context */
+    ierr = MatDuplicate( B, MAT_COPY_VALUES, &E->qty_at_b ); CHKERRQ(ierr);
+    ierr = MatDuplicate( A, MAT_COPY_VALUES, &E->ddr_at_b ); CHKERRQ(ierr);
+
+    /* clean up */
+    ierr = MatDestroy(&A); CHKERRQ(ierr);
+    ierr = MatDestroy(&B); CHKERRQ(ierr);
+
+    PetscFunctionReturn(0);
+
+}
+
+
+PetscErrorCode set_d_dr_quadratic( Ctx *E )
+{
     /* interpolate quadratic functions to get val and dval/dr at
        basic nodes.  For overlapping regions can get an estimate
        from the left and an estimate from the right, which are
@@ -309,123 +402,3 @@ PetscErrorCode set_d_dr( Ctx *E )
 
     PetscFunctionReturn(0);
 }
-
-/* below is old, and needs editting if we wish to use it
-   to build a Petsc matrix as above */
-
-/*PetscErrorCode d_dr_regular( Ctx *E, Vec in_s, Vec out_b )
-{*/
-
-    /* use staggered nodes to compute spatial derivative at basic
-       (internal) nodes.  This is 2nd order accurate for a mesh with
-       constant spacing */
-
-   /* PetscErrorCode     ierr;
-    PetscInt           i,ilo_b,ihi_b,w_b,ilo,ihi,numpts_b;
-    DM                 da_s=E->da_s,da_b=E->da_b;
-    PetscScalar        dr,*arr_out_b;
-    const PetscScalar *arr_in_s;
-
-    PetscFunctionBeginUser;
-#if (defined VERBOSE)
-    ierr = PetscPrintf(PETSC_COMM_WORLD,"d_dr\n");CHKERRQ(ierr);
-#endif*/
-    // TODO FIXME
-    //dr = E->mesh.dx_s;
-
-    /*ierr = DMDAGetCorners(da_b,&ilo_b,0,0,&w_b,0,0);CHKERRQ(ierr);
-    ihi_b = ilo_b + w_b;
-    ierr = DMDAGetInfo(E->da_b,NULL,&numpts_b,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);CHKERRQ(ierr);
-    ilo = ilo_b==0        ? 1          : ilo_b;
-    ihi = ihi_b==numpts_b ? numpts_b-1 : ihi_b;*/
-
-    // TODO: here and elsewhere, we are a little glib about assuming things abou the way the DA's partition things. We should introduce checks for any function which involves both DAs at once, that the expected ranges apply.
-
-    /* Scatter to local vectors, since we may require potentially off-processor ghost values */
-    /*ierr = DMGlobalToLocalBegin(da_s,in_s,INSERT_VALUES,E->work_local_s);CHKERRQ(ierr);
-    ierr = DMGlobalToLocalEnd(da_s,in_s,INSERT_VALUES,E->work_local_s);CHKERRQ(ierr);
-    ierr = DMDAVecGetArrayRead(da_s,E->work_local_s,&arr_in_s);CHKERRQ(ierr);
-    ierr = DMDAVecGetArray(da_b,out_b,&arr_out_b);CHKERRQ(ierr);
-    for(i=ilo; i<ihi; i++){
-        arr_out_b[i] = 1.0/dr * ( arr_in_s[i]-arr_in_s[i-1] );
-    }
-    ierr = DMDAVecRestoreArrayRead(da_s,E->work_local_s,&arr_in_s);CHKERRQ(ierr);
-    ierr = DMDAVecRestoreArray(da_b,out_b,&arr_out_b);CHKERRQ(ierr);
-
-    PetscFunctionReturn(0);
-}*/
-
-//PetscErrorCode set_d_dr2( Ctx *E )
-//{
-    /* create a matrix containing the 2nd order accurate stencil for
-       computing the 1st order derivative of a quantity at the 
-       staggered nodes
-
-       d/dr will be given by MatMult( A, x, y )
-
-       where:
-           x: (Vec) input: quantity at staggered nodes (size numpts_s)
-           y: (Vec) output: d/dr at staggered nodes (size numpts_s)
-
-       currently this matrix is global */
-
-/*    PetscErrorCode ierr;
-    PetscInt i, numpts_s, col[3], rstart, rend, ilo_s, ihi_s, w_s;
-    PetscScalar dr, value[3];
-    Mat A;
-
-    PetscFunctionBeginUser;
-
-    dr = E->mesh.dx_s;
-
-    ierr = DMDAGetCorners(E->da_s,&ilo_s,0,0,&w_s,0,0);CHKERRQ(ierr);
-    ihi_s = ilo_s + w_s;
-    ierr = DMDAGetInfo(E->da_s,NULL,&numpts_s,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);CHKERRQ(ierr);
-
-#if (defined DEBUGOUTPUT)
-    ierr = PetscPrintf(PETSC_COMM_WORLD,"set_d_dr2 : creating a matrix of size %D\n",numpts_s);CHKERRQ(ierr);
-    {
-       PetscMPIInt rank;
-       ierr = MPI_Comm_rank(PETSC_COMM_WORLD,&rank);CHKERRQ(ierr);
-       ierr = PetscPrintf(PETSC_COMM_WORLD,"[%D] set_d_dr2 : local rows %d<=i<%d\n",rank,ilo_s,ihi_s);CHKERRQ(ierr);
-    }
-#endif
-
-    ierr = MatCreate( PETSC_COMM_WORLD, &E->d_dr2 );CHKERRQ(ierr);
-    A = E->d_dr2;
-    ierr = MatSetSizes(A,PETSC_DECIDE,PETSC_DECIDE,numpts_s,numpts_s);CHKERRQ(ierr);
-    ierr = MatSetFromOptions(A);CHKERRQ(ierr);
-    ierr = MatSetUp(A);CHKERRQ(ierr);*/
-
-    /* Set first and last row values, if appropriate */
-    /*if (!ilo_s) {
-      i = 0;
-      col[0]=0; col[1]=1; col[2]=2;
-      value[0]=-3.0/2.0; value[1]=2.0; value[2]=-0.5;
-      ierr = MatSetValues(A,1,&i,3,col,value,INSERT_VALUES);CHKERRQ(ierr);
-    }
-    if (ihi_s == numpts_s) {
-      i = numpts_s-1;
-      col[0]=numpts_s-3; col[1]=numpts_s-2, col[2]=numpts_s-1;
-      value[0]=0.5; value[1]=-2.0; value[2]=3.0/2.0;
-      ierr = MatSetValues(A,1,&i,3,col,value,INSERT_VALUES);CHKERRQ(ierr);
-    }*/
-
-    /* set values corresponding to the mesh interior */
-    /*rstart = (ilo_s == 0      )  ? 1          : ilo_s;
-    rend   = (ihi_s == numpts_s) ? numpts_s-1 : ihi_s;
-    value[0]=-0.5; value[1]=0.0; value[2]=0.5;
-    for (i=rstart; i<rend; i++) {
-        col[0] = i-1; col[1] = i; col[2] = i+1;
-        ierr = MatSetValues(A,1,&i,3,col,value,INSERT_VALUES);CHKERRQ(ierr);
-    }*/
-
-    /* Assemble the matrix */
-    /*ierr = MatAssemblyBegin(A, MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
-    ierr = MatAssemblyEnd(A, MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);*/
-
-    /* Must occur after assembly */
-    /*ierr = MatScale( A, 1.0/dr);CHKERRQ(ierr);
-
-    PetscFunctionReturn(0);
-}*/
--- util.h
+++ util.h
@@ -6,5 +6,7 @@
 PetscScalar combine_matprop( PetscScalar, PetscScalar, PetscScalar );
 PetscScalar tanh_weight( PetscScalar, PetscScalar, PetscScalar );
 PetscErrorCode set_d_dr( Ctx * );
+PetscErrorCode set_d_dr_linear( Ctx * );
+PetscErrorCode set_d_dr_quadratic( Ctx * );
 
 #endif
