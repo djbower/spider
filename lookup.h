698e319c1546772236d81da7161c6dd851d5ab74
Patrick Sanan <patrick.sanan@gmail.com>
2017-09-19 10:46:31 +0200

    move lookup macros out of global defs

--- lookup.h
+++ lookup.h
@@ -4,6 +4,55 @@
 #include <petsc.h>
 #include "global_defs.h"
 
+/* 2-D datafiles containing melt and solid properties
+   as a functon of pressure and entropy */
+#define HEAD 4 /* number of header lines in datafile */
+
+/* these next values are for lookup-hires-{RT}melt */
+/* note that you must also change the file locations below! */
+#define NROWS 10100 /* number of coordinates in datafiles */
+/* N.B., NROWS = NX * NY */
+#define NX 101 /* no. of x coordinates in datafiles */
+#define NY 100 /* no. of y coordinates in datafiles */
+
+/* these next values are for lookup-hires-{RT}press */
+//#define NROWS 15100 /* number of coordinates in datafiles */
+/* N.B., NROWS = NX * NY */
+//#define NX 151 /* no. of x coordinates in datafiles */
+//#define NY 100 /* no. of y coordinates in datafiles */
+
+/* 1-D datafiles containing liquidus and solidus
+   as a function of pressure */
+#define NLS 301 /* no. of coordinates in liq and sol datafiles */
+
+/* datafile locations and material-specific constants */
+
+/* A Root Directory */
+/* https://gcc.gnu.org/onlinedocs/gcc-4.9.0/cpp/Stringification.html */
+#define STRINGIFY(x) STRINGIFY2(x)
+#define STRINGIFY2(x) #x
+#define MAGMA_ROOT_DIR_STR STRINGIFY(MAGMA_ROOT_DIR)
+
+/* liquidus/solidus data files */
+#define LIQUIDUS_STIXRUDE2009 "../../../data/lookup/lookup-fusion/2009_stixrude/RTmelt/liquidus.dat"
+#define LIQUIDUS_ANDRAULT2011 "../../../data/lookup/lookup-fusion/2011_andrault/RTmelt/liquidus.dat"
+#define SOLIDUS_STIXRUDE2009 "../../../data/lookup/lookup-fusion/2009_stixrude/RTmelt/solidus.dat"
+#define SOLIDUS_ANDRAULT2011 "../../../data/lookup/lookup-fusion/2011_andrault/RTmelt/solidus.dat"
+
+/* solid data files */
+#define ALPHA_SOL_DEFAULT "../../../data/lookup/lookup-hires-RTmelt/evo/thermal_exp_solid.dat"
+#define CP_SOL_DEFAULT "../../../data/lookup/lookup-hires-RTmelt/evo/heat_capacity_solid.dat"
+#define DTDPS_SOL_DEFAULT "../../../data/lookup/lookup-hires-RTmelt/evo/adiabat_temp_grad_solid.dat"
+#define RHO_SOL_DEFAULT "../../../data/lookup/lookup-hires-RTmelt/evo/density_solid.dat"
+#define TEMP_SOL_DEFAULT "../../../data/lookup/lookup-hires-RTmelt/evo/temperature_solid.dat"
+
+/* melt data files */
+#define ALPHA_MEL_DEFAULT "../../../data/lookup/lookup-hires-RTmelt/evo/thermal_exp_melt.dat"
+#define CP_MEL_DEFAULT "../../../data/lookup/lookup-hires-RTmelt/evo/heat_capacity_melt.dat"
+#define DTDPS_MEL_DEFAULT "../../../data/lookup/lookup-hires-RTmelt/evo/adiabat_temp_grad_melt.dat"
+#define RHO_MEL_DEFAULT "../../../data/lookup/lookup-hires-RTmelt/evo/density_melt.dat"
+#define TEMP_MEL_DEFAULT "../../../data/lookup/lookup-hires-RTmelt/evo/temperature_melt.dat"
+
 /* this structure has an x and y array size equal to NLS (see global_defs.h) */
 typedef struct _Interp1d {
     PetscScalar xa[NLS];
