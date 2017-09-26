e9b9c2c814e83adf67dd312efd2330ff15163102
Dan J Bower <djbower@gmail.com>
2017-09-26 10:54:59 +0200

    update atmosphere outputs

--- output.c
+++ output.c
@@ -21,6 +21,9 @@ PetscErrorCode atmosphere_structs_to_vec( Atmosphere const *A, AtmosphereParamet
 
     PetscErrorCode ierr;
 
+    VolatileParameters const *CO2 = &Ap->CO2_volatile_parameters;
+    VolatileParameters const *H2O = &Ap->H2O_volatile_parameters;
+
     PetscFunctionBeginUser;
 
     ierr = VecSetValue(vec,0,Ap->MODEL,INSERT_VALUES);CHKERRQ(ierr);
@@ -30,31 +33,32 @@ PetscErrorCode atmosphere_structs_to_vec( Atmosphere const *A, AtmosphereParamet
     ierr = VecSetValue(vec,4,Ap->teqm,INSERT_VALUES);CHKERRQ(ierr);
     ierr = VecSetValue(vec,5,Ap->PARAM_UTBL,INSERT_VALUES);CHKERRQ(ierr);
     ierr = VecSetValue(vec,6,Ap->param_utbl_const,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,7,Ap->P0,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,8,Ap->H2O_volatile_parameters.initial,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,9,Ap->H2O_volatile_parameters.kdist,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,10,Ap->H2O_volatile_parameters.kabs,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,11,Ap->H2O_volatile_parameters.henry,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,12,Ap->H2O_volatile_parameters.henry_pow,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,13,Ap->CO2_volatile_parameters.initial,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,14,Ap->CO2_volatile_parameters.kdist,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,15,Ap->CO2_volatile_parameters.kabs,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,16,Ap->CO2_volatile_parameters.henry,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,17,Ap->CO2_volatile_parameters.henry_pow,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,18,A->Mliq,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,19,A->Msol,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,20,A->dMliqdt,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,21,A->tsurf,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,22,A->tau,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,23,A->p0,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,24,A->dp0dx,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,25,A->m0,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,26,A->tau0,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,27,A->p1,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,28,A->dp1dx,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,29,A->m1,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,30,A->tau1,INSERT_VALUES);CHKERRQ(ierr);
-    ierr = VecSetValue(vec,31,A->emissivity,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,7,Ap->SOLVE_FOR_VOLATILES,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,8,Ap->P0,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,9,CO2->initial,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,10,CO2->kdist,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,11,CO2->kabs,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,12,CO2->henry,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,13,CO2->henry_pow,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,14,H2O->initial,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,15,H2O->kdist,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,16,H2O->kabs,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,17,H2O->henry,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,18,H2O->henry_pow,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,19,A->Mliq,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,20,A->Msol,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,21,A->dMliqdt,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,22,A->tsurf,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,23,A->tau,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,24,A->p0,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,25,A->dp0dx,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,26,A->m0,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,27,A->tau0,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,28,A->p1,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,29,A->dp1dx,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,30,A->m1,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,31,A->tau1,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecSetValue(vec,32,A->emissivity,INSERT_VALUES);CHKERRQ(ierr);
 
     ierr = VecAssemblyBegin(vec);CHKERRQ(ierr);
     ierr = VecAssemblyEnd(vec);CHKERRQ(ierr);
