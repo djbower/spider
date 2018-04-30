0092c8ba80d9eb3f5ad3dc5d2077dbb64581820d
Dan J Bower <djbower@gmail.com>
2018-04-30 11:56:33 +0200

    plot atmosphere summary figure (figure 6) using json output

--- plot_spider.py
+++ plot_spider.py
@@ -5,6 +5,7 @@ import logging
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
+import matplotlib.transforms as transforms
 import numpy as np
 import os
 import sys
@@ -72,7 +73,10 @@ class MyJSON( object ):
         '''get the scaled values for a particular field'''
         fdata_d = self.get_field_data( field )
         scaling = fdata_d['scaling']
-        values_a = np.array( [float(value) for value in fdata_d['values']] )
+        if len( fdata_d['values'] ) == 1:
+            values_a = float( fdata_d['values'][0] )
+        else:
+            values_a = np.array( [float(value) for value in fdata_d['values']] )
         scaled_values_a = scaling * values_a
         if fmt_o:
             scaled_values_a = ascale( scaled_values_a, fmt_o.const )
@@ -891,50 +895,54 @@ def bower_et_al_2018_fig1( args ):
 #====================================================================
 def figure6( args ):
 
+    logger.info( 'building atmosphere' )
+
     width = 4.7747
     height = 4.7747
-    fig_o = FigureData( args, 2, 2, width, height )
+    fig_o = FigureData( args, 2, 2, width, height, 'atmosphere' )
 
     ax0 = fig_o.ax[0][0]
     ax1 = fig_o.ax[0][1]
     ax2 = fig_o.ax[1][0]
     ax3 = fig_o.ax[1][1]
 
-    data = atmosphere_data_to_array( fig_o )
+    fig_o.time = get_all_output_times()
+    timeMyr_a = fig_o.time * 1.0E-6 # Myrs
 
-    # bit clunky, but works for now
-    time = data[:,0] * 1.0E-6 # in Myrs
+    mass_liquid_a = get_single_values_for_times( 'mass_liquid', fig_o.time )
+    mass_solid_a = get_single_values_for_times( 'mass_solid', fig_o.time )
+    mass_mantle_a = get_single_values_for_times( 'mass_mantle', fig_o.time )
+    mass_mantle = mass_mantle_a[0] # time independent
 
-    Mliq = data[:,1]
-    Msol = data[:,2]
-    Mtot = data[:,3]
-    tsurf = data[:,4]
-    tau = data[:,5]
-    emissivity = data[:,6]
-    CO2liq = data[:,7]
-    CO2sol = data[:,8]
-    CO2atm = data[:,9]
-    CO2tot = data[:,10]
-    CO2p = data[:,11]
-    CO2dpdx = data[:,12]
-    CO2tau = data[:,13]
-    H2Oliq = data[:,14]
-    H2Osol = data[:,15]
-    H2Oatm = data[:,16]
-    H2Otot = data[:,17]
-    H2Op = data[:,18]
-    H2Odpdx = data[:,19]
-    H2Otau = data[:,20]
+    CO2_initial_a = get_single_values_for_times( 'CO2_initial', fig_o.time )
+    CO2_liquid_a = get_single_values_for_times( 'CO2_liquid', fig_o.time )
+    CO2_solid_a = get_single_values_for_times( 'CO2_solid', fig_o.time )
+    CO2_atmos_a = get_single_values_for_times( 'CO2_atmosphere', fig_o.time )
 
-    # mass fractions
-    CO2liq /= CO2tot
-    CO2sol /= CO2tot
-    CO2atm /= CO2tot
+    H2O_initial_a = get_single_values_for_times( 'H2O_initial', fig_o.time )
+    H2O_liquid_a = get_single_values_for_times( 'H2O_liquid', fig_o.time )
+    H2O_solid_a = get_single_values_for_times( 'H2O_solid', fig_o.time )
+    H2O_atmos_a = get_single_values_for_times( 'H2O_atmosphere', fig_o.time )
 
-    # mass fractions
-    H2Oliq /= H2Otot
-    H2Osol /= H2Otot
-    H2Oatm /= H2Otot
+    # compute total mass (kg) in each reservoir
+    CO2_liquid_kg_a = ppm_to_mass_fraction( CO2_liquid_a ) * mass_liquid_a
+    CO2_solid_kg_a = ppm_to_mass_fraction( CO2_solid_a ) * mass_solid_a
+    CO2_total_kg_a = ppm_to_mass_fraction( CO2_initial_a ) * mass_mantle
+    CO2_total_kg = CO2_total_kg_a[0] # time-independent
+    # TODO: below mass is conserved by definition, but can also
+    # compute directly from partial pressure
+    CO2_atmos_kg_a = CO2_total_kg - CO2_liquid_kg_a - CO2_solid_kg_a
+
+    H2O_liquid_kg_a = ppm_to_mass_fraction( H2O_liquid_a ) * mass_liquid_a
+    H2O_solid_kg_a = ppm_to_mass_fraction( H2O_solid_a ) * mass_solid_a
+    H2O_total_kg_a = ppm_to_mass_fraction( H2O_initial_a ) * mass_mantle
+    H2O_total_kg = H2O_total_kg_a[0] # time-independent
+    # TODO: below mass is conserved by definition, but can also
+    # compute directly from partial pressure
+    H2O_atmos_kg_a = H2O_total_kg - H2O_liquid_kg_a - H2O_solid_kg_a
+
+    temperature_surface_a = get_single_values_for_times( 'temperature_surface', fig_o.time )
+    emissivity_a = get_single_values_for_times( 'emissivity', fig_o.time )
 
     xticks = [1E-2,1E-1,1E0,1E1,1E2]
     xlabel = 'Time (Myr)'
@@ -942,79 +950,64 @@ def figure6( args ):
     red = fig_o.get_color(3)
     blue = fig_o.get_color(-3)
 
-    ### new to make regime plot
-    yy = Mliq/Mtot
-    xx = time
-    #cont_l = np.linspace(0.2,0.8,3)
-    cont_l = [0.2,0.8]#,0.8]
-    val_l = []
-    for cont in cont_l:
-        # this is probbaly broken now
-        item, yval = find_value( xx, yy, cont )
-        val_l.append( yval )
+    # to plot melt fraction contours on figure (a)
+    # compute time at which desired melt fraction is reached
+    phi_a = mass_liquid_a / mass_mantle
+    phi_cont_l = [0.2, 0.8] # 20% and 80% melt fraction
+    phi_time_l = [] # contains the times for each contour
+    for cont in phi_cont_l:
+        time_temp_l = find_value( timeMyr_a, phi_a, cont )
+        index = get_first_non_zero_index( time_temp_l )
+        phi_time_l.append( timeMyr_a[index] )
 
+    ##########
     # figure a
+    ##########
     title = '(a) Partial pressure (bar)'
     ylabel = '$p$'
-    # for testing
-    #h3, = ax0.semilogx( time, 400*Mliq/Mtot, 'k--' )
-    for cc, cont in enumerate(cont_l):
-        h3, = ax0.semilogx( [val_l[cc],val_l[cc]],[0,400], color='0.25', linestyle=':')
-        label = 100*cont
-        ax0.text( val_l[cc], 400, '%(label)0d' % vars(), ha='center' )#, rotation='vertical' )
-        # for massive atmosphere
-        #h3, = ax0.semilogx( [val_l[cc],val_l[cc]],[0,4000], color='0.25', linestyle=':')
-        #ax0.text( val_l[cc], 4000, '%(label)0d' % vars(), ha='center' )#, rotation='vertical' )
-    #phishift = val_l[cc] - 1.8 #0.135
-    #ax0.text( phishift, 4000, '$\phi (\%)$' )
-    phishift = val_l[cc] - 0.135
-    ax0.text( phishift, 400, '$\phi (\%)$' )
-    h1, = ax0.semilogx( time, CO2p*1.0E-5, color=red, linestyle='-', label='CO$_2$')
-    h2, = ax0.semilogx( time, H2Op*1.0E-5, color=blue, linestyle='-', label='H$_2$O')
+    trans = transforms.blended_transform_factory(
+        ax0.transData, ax0.transAxes)
+    for cc, cont in enumerate(phi_cont_l):
+        ax0.axvline( phi_time_l[cc], ymin=0.1, ymax=0.9, color='0.25', linestyle=':' )
+        label = int(cont*100) # as percent
+        ax0.text( phi_time_l[cc], 0.9, '{:2d}'.format(label), va='bottom', ha='center', transform=trans )
+    ax0.text( 0.1, 0.9, '$\phi (\%)$', ha='center', va='bottom', transform=ax0.transAxes )
+    h1, = ax0.semilogx( timeMyr_a, CO2_atmos_a, color=red, linestyle='-', label='CO$_2$')
+    h2, = ax0.semilogx( timeMyr_a, H2O_atmos_a, color=blue, linestyle='-', label='H$_2$O')
     handle_l = [h1,h2]
     fig_o.set_myaxes( ax0, title=title, ylabel=ylabel, xticks=xticks )
-    fig_o.set_mylegend( ax0, handle_l, loc='center right', ncol=1, TITLE="" )
+    fig_o.set_mylegend( ax0, handle_l, loc='center left', ncol=1, TITLE="" )
     ax0.yaxis.set_label_coords(-0.1,0.575)
 
+    ##########
     # figure b
+    ##########
     title = '(b) Volatile reservoirs'
-    #h5, = ax0.semilogx( time, Mliq/Mtot, 'k--', label='melt' )
-    h1, = ax1.semilogx( time, CO2liq+CO2sol, color=red, linestyle='-', label='Magma' )
-    #h2, = ax0.semilogx( time, CO2sol, 'b-', label='CO2 sol' )
-    h2, = ax1.semilogx( time, CO2atm, color=red, linestyle='--', label='Atmos' )
-    h3, = ax1.semilogx( time, H2Oliq+H2Osol, color=blue, linestyle='-')#, label='H2O magma' )
-    #h5, = ax0.semilogx( time, H2Osol, 'b--', label='H2O sol' )
-    h4, = ax1.semilogx( time, H2Oatm, color=blue, linestyle='--')#, label='H2O atmos' )
-    #ax0.semilogx( time, CO2liq+CO2sol+CO2atm, 'k-' )
+    #h5, = ax1.semilogx( timeMyr_a, mass_liquid_a / mass_mantle, 'k--', label='melt' )
+    h1, = ax1.semilogx( timeMyr_a, (CO2_liquid_kg_a+CO2_solid_kg_a) / CO2_total_kg, color=red, linestyle='-', label='Magma' )
+    h2, = ax1.semilogx( timeMyr_a, CO2_atmos_kg_a / CO2_total_kg, color=red, linestyle='--', label='Atmos' )
+    h3, = ax1.semilogx( timeMyr_a, (H2O_liquid_kg_a+H2O_solid_kg_a) / H2O_total_kg, color=blue, linestyle='-', label='Magma' )
+    h4, = ax1.semilogx( timeMyr_a, H2O_atmos_kg_a / H2O_total_kg, color=blue, linestyle='--', label='Atmos')
     fig_o.set_myaxes( ax1, title=title, ylabel='$x$', xticks=xticks )
-    handle_l = [h1,h2]#,h3,h4,h5]
+    handle_l = [h1,h2]
     fig_o.set_mylegend( ax1, handle_l, loc='center left', ncol=1, TITLE="" )
     ax1.yaxis.set_label_coords(-0.1,0.46)
 
+    ##########
     # figure c
-    #title = '(c) H$_2$O reservoirs'
-    #h4, = ax2.semilogx( time, Mliq/Mtot, 'k--', label='Melt frac' )
-    #h1, = ax2.semilogx( time, H2Oliq, 'r-', label='Liquid' )
-    #h2, = ax2.semilogx( time, H2Osol, 'b-', label='Solid' )
-    #h3, = ax2.semilogx( time, H2Oatm, 'g-', label='Atmos' )
-    #ax2.semilogx( time, H2Oliq+H2Osol+H2Oatm, 'k-' )
-    #fig_o.set_myaxes( ax2, title=title, xlabel=xlabel, ylabel='$x$', xticks=xticks )
-    #handle_l = [h1,h3,h2,h4]
-    #fig_o.set_mylegend( ax2, handle_l, loc='center left', ncol=1, TITLE=0 )
-    #ax2.yaxis.set_label_coords(-0.1,0.46)
-
-    # figure c
+    ##########
     title = '(c) Surface temperature'
     ylabel = '$T$'
-    ax2.semilogx( time, tsurf, 'k-' )
+    ax2.semilogx( timeMyr_a, temperature_surface_a, 'k-' )
     fig_o.set_myaxes( ax2, title=title, xlabel=xlabel, ylabel=ylabel, xticks=xticks )
     ax2.yaxis.set_label_coords(-0.1,0.5)
-    plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 
+    ##########
     # figure d
+    ##########
     title = '(d) Emissivity'
     ylabel = '$\epsilon$'
-    ax3.semilogx( time, emissivity, 'k-' )
+    ax3.semilogx( timeMyr_a, emissivity_a, 'k-' )
     fig_o.set_myaxes( ax3, title=title, xlabel=xlabel, ylabel=ylabel, xticks=xticks )
     ax3.yaxis.set_label_coords(-0.1,0.55)
     plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
@@ -1023,6 +1016,8 @@ def figure6( args ):
 
 #====================================================================
 def figure7( args ):
+
+    # TODO: needs updating to use json output
 
     '''Extract magma ocean depth for partitioning of moderately
        siderophile elements'''
@@ -1090,6 +1085,11 @@ def figure7( args ):
     #ax1.yaxis.set_label_coords(-0.075,0.59)
 
     fig_o.savefig(7)
+
+#====================================================================
+def ppm_to_mass_fraction( ppm ):
+
+    return ppm / 1.0E6
 
 #====================================================================
 def get_mo_PT_conditions( fig_o, time_l, field1, criteria, label="", color='black' ):
@@ -1190,6 +1190,8 @@ def get_mo_PT_conditions( fig_o, time_l, field1, criteria, label="", color='blac
 #====================================================================
 def figure8( args ):
 
+    # TODO: needs updating to use json output
+
     width = 4.7747#/2
     height = 4.7747/2
     fig_o = FigureData( args, 1, 2, width, height )
@@ -1274,7 +1276,27 @@ def sign_change( a ):
     return s
 
 #====================================================================
-def atmosphere_data_to_array( fig_o ):
+def get_single_values_for_times( field, time_l ):
+
+    data_l = []
+
+    for nn, time in enumerate( time_l ):
+
+        # read json
+        myjson_o = MyJSON( 'output/{}.json'.format(time) )
+
+        yy = myjson_o.get_scaled_field_values( field )
+        data_l.append( yy )
+
+    data_a = np.array( data_l )
+
+    return data_a
+
+#====================================================================
+def get_all_output_times():
+
+    '''get all times (in Myrs) from the json files located in the
+       output directory'''
 
     # locate times to process based on files located in output/
     odir = 'output'
@@ -1283,42 +1305,14 @@ def atmosphere_data_to_array( fig_o ):
         print('ERROR: output directory contains no files')
         sys.exit(1)
 
-    time_l = [fname.split('.')[-1] for fname in file_l]
-
-    # remove '.info' suffix
-    time_l = list(filter(lambda a: a !='info', time_l))
-    # remove '.m' suffix
-    time_l = list(filter(lambda a: a !='m', time_l))
-    # remove gitignore
-    time_l = list(filter(lambda a: a !='gitignore', time_l))
-    # convert to int
-    time_l = [int(val) for val in time_l]
+    time_l = [fname for fname in file_l]
+    time_l = list(filter(lambda a: a.endswith('json'), time_l))
+    time_l = [int(time.split('.json')[0]) for time in time_l]
     # ascending order
     time_l = sorted( time_l, key=int)
+    time_a = np.array( time_l )
 
-    data = fig_o.get_data( 'atmosphere_data', time_l[0] )
-
-    # must be some data to process
-    if data is None:
-        print('atmosphere_data_to_array: no data')
-        sys.exit(0)
-
-    ncols = np.size( data ) + 1
-    nrows = len( time_l )
-
-    data_a = np.zeros( (nrows, ncols+2) )
-
-    for nn, time in enumerate( time_l ):
-
-        data = fig_o.get_data( 'atmosphere_data', time )
-        data_a[nn,0] = time
-        data_a[nn,1:-2] = data
-
-        dSdr_b_aug = fig_o.get_data( 'dSdr_b_aug', time )
-        data_a[nn,-2] = dSdr_b_aug[0] # x0
-        data_a[nn,-1] = dSdr_b_aug[1] # x1
-
-    return data_a
+    return time_a
 
 #====================================================================
 def ascale( yy, constant ):
@@ -1371,110 +1365,6 @@ def sci_notation(num, decimal_digits=1, precision=None, exponent=None):
 
     return fmt
     #return r"${0:.{2}f}\cdot10^{{{1:d}}}$".format(coeff, exponent, precision)
-
-#====================================================================
-
-# REMOVE
-
-#def get_vector_index( instring ):
-#    """
-#    the order of fields in data_l must match exactly the order in
-#    which PetscVecs are written to the output Petsc binary.
-#    See monitor.c
-#    """
-
-#    data_l = [
-        # extraneous
-#        'time_data',
-#        'constant_data',
-#        'atmosphere_data',
-#        'dSdr_b_aug',
-#        # stored in M->meshVecs_b
-#        'area_b',
-#        'dPdr_b',
-#        'pressure_b',
-#        'radius_b',
-#        'mix_b',
-#        # stored in M->meshVecs_s
-#        'pressure_s',
-#        'radius_s',
-#        'volume_s',
-#        'dPdr_s',
-#        'area_s',
-#        'rho_s',
-#        'mass_s',
-#        # stored in S->solutionVecs_b
-#        'alpha',
-#        'alpha_mix',
-#        'cond',
-#        'cp',
-#        'cp_mix',
-#        'dfusdr',
-#        'dfusdr_temp',
-#        'dSdr',
-#        'dSliqdr',
-#        'dSsoldr',
-#        'dTdrs',
-#        'dTdrs_mix',
-#        'Etot',
-#        'fusion',
-#        'fusion_curve',
-#        'fusion_curve_temp',
-#        'fusion_rho',
-#        'fusion_temp',
-#        'fwtl',
-#        'fwts',
-#        'gphi',
-#        'gsuper',
-#        'Jcond',
-#        'Jconv',
-#        'Jgrav',
-#        'Jmix',
-#        'Jtot',
-#        'kappah',
-#        'liquidus',
-#        'liquidus_rho',
-#        'liquidus_temp',
-#        'nu',
-#        'phi',
-#        'regime',
-#        'rho',
-#        'S',
-#        'solidus',
-#        'solidus_rho',
-#        'solidus_temp',
-#        'temp',
-#        'visc',
-         # stored in S->solutionVecs_s
-#        'cp_s',
-#        'cp_mix_s',
-#        'dSdt_s',
-#        'fusion_s',
-#        'fusion_curve_s',
-#        'fusion_curve_temp_s',
-#        'fusion_temp_s',
-#        'fwtl_s',
-#        'fwts_s',
-#        'gphi_s',
-#        'Hradio_s',
-#        'Htidal_s',
-#        'Htot_s',
-#        'lhs_s',
-#        'liquidus_rho_s',
-#        'liquidus_s',
-#        'liquidus_temp_s',
-#        'phi_s',
-#        'rho_s',
-#        'S_s',
-#        'solidus_s',
-#        'solidus_rho_s',
-#        'solidus_temp_s',
-#        'temp_s'
-#    ]
-
-#    index = data_l.index( instring )
-
-#    return index
 
 #====================================================================
 def main():
