8eb3d05135b85b90bbe2dda0a190c24345e4f0a3
Dan J Bower <djbower@gmail.com>
2018-09-02 19:54:38 +0200

    add Mg_Si0 and Mg_Si1 to parameters structure

--- parameters.h
+++ parameters.h
@@ -165,6 +165,8 @@ typedef struct _Parameters {
     PetscScalar activation_energy_sol;
     PetscScalar activation_volume_sol;
     PetscScalar viscosity_temperature_offset_sol;
+    PetscScalar Mg_Si0;
+    PetscScalar Mg_Si1;
     PetscScalar cond_sol;
     PetscScalar log10visc_mel;
     PetscScalar cond_mel;
