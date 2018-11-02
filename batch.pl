49d345a86678a9be32b759059d4941b483c0d719
Rob Spaargaren <rjspaargaren@guests-wired-nw-013.ethz.ch>
2018-11-02 14:05:44 +0100

    Addition of vicous lid

--- batch.pl
+++ batch.pl
@@ -1,10 +1,7 @@
-<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<document type="com.apple.InterfaceBuilder3.CocoaTouch.XIB" version="3.0" toolsVersion="11134" systemVersion="15F34" targetRuntime="iOS.CocoaTouch" propertyAccessControl="none" useAutolayout="YES" useTraitCollections="YES" colorMatched="YES">
-    <dependencies>
-        <plugIn identifier="com.apple.InterfaceBuilder.IBCocoaTouchPlugin" version="11106"/>
-    </dependencies>
-    <objects>
-        <placeholder placeholderIdentifier="IBFilesOwner" id="-1" userLabel="File's Owner"/>
-        <placeholder placeholderIdentifier="IBFirstResponder" id="-2" customClass="UIResponder"/>
-    </objects>
-</document>
+for j in `find /Users/rjspaargaren/Documents/Thesis/Code_Dan/spider-dev/Thesis_data/lid/* -name "n*" -type d -exec basename {} \;`;
+do echo $j;
+folname=$j;
+cd /Users/rjspaargaren/Documents/Thesis/Code_Dan/spider-dev/Thesis_data/lid/$folname;
+mkdir output;
+spider -options_file bu_input.opts;
+done;
