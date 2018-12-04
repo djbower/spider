2421fc9a79743a05b554d6d025138cdd2f2ab50d
Dan J Bower <djbower@gmail.com>
2018-12-04 17:31:21 +0100

    improve clarity of plotting, and add polynomial plotting functionality

--- py3/plot_rheological_transition.py
+++ py3/plot_rheological_transition.py
@@ -21,6 +21,7 @@ def figure9():
     PMIN = 1.0 # GPa
     PMAX = 135.0 # GPa
     PHI = 0.4 # melt fraction contour, or -1 for dynamic
+    TPRIME1_PRESSURE = 5.0 # pressure at which rheological transition is assumed to be at the surface
     #---------------
 
     width = 4.7747
@@ -55,7 +56,7 @@ def figure9():
         else:
             # melt fraction criteria
             yy1 = myjson_o.get_scaled_field_values( 'phi_b' )
-            yy2 = 0.4
+            yy2 = PHI
 
         # mantle temperature (vector)
         mantle_temp = myjson_o.get_scaled_field_values( 'temp_b' )
@@ -69,42 +70,45 @@ def figure9():
         # find the pressure(s) that correspond(s) to the cross-over point
         pres = su.find_xx_for_yy( xx_pres_b, yy1, yy2 )
         ind = su.get_first_non_zero_index( pres )
-        if ind is not None:
+        print( 'ind=', ind )
+        if ind is not None: # before rheological transition reaches CMB
+            # save the time at which the rheological transition first
+            # reaches the CMB
+            try:
+                tprime0
+            except NameError:
+                tprime0 = time
+                tprime0ii = ii
             mantle_rheological_pres = xx_pres_b[ind]
+            if mantle_rheological_pres < TPRIME1_PRESSURE:
+                try:
+                    tprime1
+                except NameError:
+                    tprime1 = time
+                    tprime1ii = ii
             mantle_rheological_temp = mantle_temp[ind]
         else:
             mantle_rheological_pres = xx_pres_b[-1]
             mantle_rheological_temp = mantle_temp[-1]
 
+        # time is appended twice, because the second time will be corrected for the offset
         data_l.append( ( time, time, mantle_rheological_pres, mantle_rheological_temp,
             mantle_surface_temp, mantle_surface_flux ) )
 
-        # 'None' is returned until the rheological transition first
-        # reaches the CMB
-        #if ind is not None:
-            # time is appended twice, because the second time will be
-            # corrected for the offset
-        #    data_l.append( (time, time, xx_pres_b[ind], y_temp[ind]) )
-
     # reshape into numpy array
-    # columns: time, time-offset, pressure, temperature
     data_a = np.reshape( data_l, (-1,6) )
     # tprime0 defines the time at which the rheological front is at the CMB
-    tprime0 = data_a[0,0]
+    #tprime0 = data_a[0,0]
     data_a[:,1] -= tprime0
 
     # ---------------------------
     # ---- surface heat flux ----
-    #surff_a = np.column_stack( (fig_o.time, fluxsurf_l) )
-    #surff0_a = surff_a[:,0]
-    #surff1_a = surff_a[:,1]
-
     trans = transforms.blended_transform_factory(ax0.transData, ax0.transAxes)
     h1, = ax0.semilogy( data_a[:,0], data_a[:,5], marker='o', markersize=4.0, color='0.8' )
-    #ax2.axvline( tprime0, ymin=0.1, ymax=0.8, color='0.25', linestyle=':')
-    #ax2.text( tprime0, 0.82, '$t^\prime_0$', ha='right', va='bottom', transform = trans )
-    #ax2.axvline( tprime1abs, ymin=0.1, ymax=0.8, color='0.25', linestyle=':')
-    #ax2.text( tprime1abs, 0.82, '$t^\prime_1$', ha='right', va='bottom', transform = trans )
+    ax0.axvline( tprime0, ymin=0.1, ymax=0.8, color='0.25', linestyle=':')
+    ax0.text( tprime0, 0.82, '$t^\prime_0$', ha='right', va='bottom', transform = trans )
+    ax0.axvline( tprime1, ymin=0.1, ymax=0.8, color='0.25', linestyle=':')
+    ax0.text( tprime1, 0.82, '$t^\prime_1$', ha='right', va='bottom', transform = trans )
     title = r'(a) $q_0(t)$, W/m$^2$'
     fig_o.set_myaxes( ax0, title=title, ylabel='$q_0$', xlabel='$t$ (yrs)' )
     ax0.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
@@ -114,23 +118,12 @@ def figure9():
 
     # -----------------------------
     # ---- surface temperature ----
-    #surft_a = np.column_stack( (fig_o.time, tempsurf_l) )
-    #ind_tcut = surft_a[:,1] > 1200.0
-    #surft0_a = surft_a[:,0]#[ind_tcut] # time
-    #surft1_a = surft_a[:,1]#[ind_tcut] # surface temperature
-
     trans = transforms.blended_transform_factory(ax1.transData, ax1.transAxes)
     ax1.plot( data_a[:,0], data_a[:,4], marker='o', markersize=4.0, color='0.8' )
-
-
-    #surft_fit = np.exp( log_func_Psi_C(expon)( time_rf_a, *popt2 ) )
-    #h1, = ax3.plot( tprime0 + time_rf_a, surft_fit, ':', color='blue', linewidth=2, label=r'Fit2' )
-    #surft_fit2 = np.exp( log_func_Psi_C(expon)( time_rf_abs_a, *popt ) )
-    #h2, = ax3.plot( time_rf_abs_a, surft_fit2, '-', color='black', linewidth=2, label=r'Fit')
-    #ax3.axvline( tprime0, ymin=0.1, ymax=0.8, color='0.25', linestyle=':')
-    #ax3.text( tprime0, 0.82, '$t^\prime_0$', ha='right', va='bottom', transform = trans )
-    #ax3.axvline( tprime1abs, ymin=0.1, ymax=0.8, color='0.25', linestyle=':')
-    #ax3.text( tprime1abs, 0.82, '$t^\prime_1$', ha='right', va='bottom', transform = trans )
+    ax1.axvline( tprime0, ymin=0.1, ymax=0.8, color='0.25', linestyle=':')
+    ax1.text( tprime0, 0.82, '$t^\prime_0$', ha='right', va='bottom', transform = trans )
+    ax1.axvline( tprime1, ymin=0.1, ymax=0.8, color='0.25', linestyle=':')
+    ax1.text( tprime1, 0.82, '$t^\prime_1$', ha='right', va='bottom', transform = trans )
     title = r'(b) $T0_m(t)$, K'
     yticks = [1500,2000,2500,3000,3500,4000]
     fig_o.set_myaxes( ax1, title=title, ylabel='$T0_m$', xlabel='$t$ (yrs)', yticks=yticks )
@@ -142,42 +135,46 @@ def figure9():
 
     # ---------------------------
     # ---- rheological front ----
-    #ind_pcut = np.where( (data_a[:,1]<PMAX) & (data_a[:,1]>PMIN) )[0]
-    # time offset has already been applied, so time is 0 at t'=0
-    #time_a = data_a[:,1][ind_pcut] # time with offset
-    #pres_a = data_a[:,2][ind_pcut] # pressure
-    #temp_a = data_a[:,3][ind_pcut] # temperature
-    # save data to text files for plotting
     #if EXPORT:
     #    np.savetxt( 'Pr.dat', np.column_stack((time_a,pres_a)))
     #    np.savetxt( 'Tr.dat', np.column_stack((time_a,temp_a)))
-
-
-    # indices for plotting
-    #indp = np.where( data_a[:,1] > 1.0 )[0]
-    #tmax = np.max( data_a[:,0][indp] )
-    #tmin = np.min( data_a[:,0][indp] )
-    #vmax = np.max( data_a[:,0][indp] )
-    #vmin = np.min( data_a[:,0][indp] )
-
     ax2.plot( data_a[:,1], data_a[:,2], marker='o', markersize=4.0, color='0.8' )
-    #Pf_fit = pressure_from_time_rheological_front( data_a[:,0][indp], expon, Psi, C, lambdaa, T0c )
-    #h2, = ax0.plot( data_a[:,0][indp], Pf_fit, '-', color='black', linewidth=2, label=r'Fit' )
     title = r'(c) $P_f(t^\prime)$, GPa'
     yticks = [0,50,100,150]
     fig_o.set_myaxes( ax2, title=title, ylabel='$P_f$', xlabel='$t^\prime$ (yrs)', yticks=yticks )
     ax2.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
     ax2.yaxis.set_label_coords(-0.2,0.5)
+    ax2.set_xlim( (0, tprime1-tprime0) )
 
     ax3.plot( data_a[:,1], data_a[:,3], marker='o', markersize=4.0, color='0.8' )
-    #Tf_fit = temperature_from_time_rheological_front( data_a[:,0][indp], expon, Psi, C, lambdaa, T0c, alpha )
-    #h2, = ax1.plot( data_a[:,0][indp], Tf_fit, '-', color='black', linewidth=2, label=r'Fit' )
     title = r'(d) $T_f(t^\prime)$, K'
     yticks = [2000,3000,4000]
     fig_o.set_myaxes( ax3, title=title, ylabel='$T_f$', yticks=yticks, xlabel='$t^\prime$ (yrs)' )
     ax3.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
     ax3.set_ylim( [1500,4500] )
     ax3.yaxis.set_label_coords(-0.2,0.575)
+    ax3.set_xlim( (0, tprime1-tprime0) )
+
+    ###################
+    ##### fitting #####
+    ###################
+
+    # linear fit to pressure
+    deg = 2
+    xx = data_a[tprime0ii:tprime1ii,0]-tprime0
+    yy = data_a[tprime0ii:tprime1ii,2]
+    rheological_pres_poly = np.polyfit( xx, yy, deg )
+    rheological_pres_o = np.poly1d( rheological_pres_poly )
+    rheological_pres_fit = rheological_pres_o( data_a[:,1] )
+    ax2.plot( data_a[:,1], rheological_pres_fit )
+
+    # linear fit to temperature
+    deg = 2
+    yy = data_a[tprime0ii:tprime1ii,3]
+    rheological_temp_poly = np.polyfit( xx, yy, deg )
+    rheological_temp_o = np.poly1d( rheological_temp_poly )
+    rheological_temp_fit = rheological_temp_o( data_a[:,1] )
+    ax3.plot( data_a[:,1], rheological_temp_fit )
 
     fig_o.savefig(9)
 
