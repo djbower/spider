d62425533b5699087abac805a4ecaaa573350f26
Patrick Sanan <patrick.sanan@gmail.com>
2018-12-18 17:04:54 +0100

    Tests: move script to tests/ and minor cleanup

--- tests/blackbody/tests.py
+++ tests/blackbody/tests.py
@@ -2,19 +2,13 @@ import os
 import re
 import pyTestHarness.test as pthtest
 
-# Note: there is a lot of code duplication here. This should be addressed
-# before copying this same logic any more
-
 def blackbody(rootDir) :
   thisDir = os.path.split(os.path.abspath(__file__))[0]
   testName = "blackbody"
   ranks = 1
-  # Run the test,
-  # then use a local script to generate a text file to compare with,
-  # then run the plotting script (output not checked)
   launch = [\
           os.path.join(rootDir,'spider')  + ' -options_file ' + os.path.join(rootDir,'examples','bower_2019','blackbody','bu_input.opts'),\
-          os.path.join(thisDir,'json_timestep_to_txt.py 100'),
+          os.path.join(rootDir,'tests','json_timestep_to_txt.py 100'),
           #os.path.join(rootDir,'tests','timeout.sh') + ' -t 6 ' + os.path.join(rootDir,'py3','plot_spider.py') + ' -t 0,100,200,400,800,1200,1500',\
           ]
 
@@ -24,25 +18,20 @@ def blackbody(rootDir) :
       t.compareFloatingPointRelative(re.escape('scaling: '),1e-14)
       t.compareFloatingPointRelative(re.escape('val: '),    1e-5)
 
-  # Create Test Object
   t = pthtest.Test(testName,ranks,launch,expectedFile)
   t.setComparisonFile('out.txt')
   t.setVerifyMethod(comparefunc)
   t.setWalltime(10) # minutes
   t.setUseSandbox()
-
   return(t)
 
 def blackbody_init_liquid(rootDir) :
   thisDir = os.path.split(os.path.abspath(__file__))[0]
   testName = "blackbody_init_liquid"
   ranks = 1
-  # Run the test,
-  # then use a local script to generate a text file to compare with,
-  # then run the plotting script (output not checked)
   launch = [\
           os.path.join(rootDir,'spider')  + ' -options_file ' + os.path.join(rootDir,'examples','bower_2019','blackbody','bu_input.opts') + ' -ic_adiabat_entropy 2700' + ' -nstepsmacro 0',\
-          os.path.join(thisDir,'json_timestep_to_txt.py'),
+          os.path.join(rootDir,'tests','json_timestep_to_txt.py'),
           #os.path.join(rootDir,'tests','timeout.sh') + ' -t 6 ' + os.path.join(rootDir,'py3','plot_spider.py') + ' -t 0',\
           ]
 
@@ -52,7 +41,6 @@ def blackbody_init_liquid(rootDir) :
       t.compareFloatingPointRelative(re.escape('scaling: '),1e-14)
       t.compareFloatingPointRelative(re.escape('val: '),    1e-5)
 
-  # Create Test Object
   t = pthtest.Test(testName,ranks,launch,expectedFile)
   t.setComparisonFile('out.txt')
   t.setVerifyMethod(comparefunc)
@@ -65,12 +53,9 @@ def blackbody_init_mixed(rootDir) :
   thisDir = os.path.split(os.path.abspath(__file__))[0]
   testName = "blackbody_init_mixed"
   ranks = 1
-  # Run the test,
-  # then use a local script to generate a text file to compare with,
-  # then run the plotting script (output not checked)
   launch = [\
           os.path.join(rootDir,'spider')  + ' -options_file ' + os.path.join(rootDir,'examples','bower_2019','blackbody','bu_input.opts') + ' -ic_adiabat_entropy 1900' + ' -nstepsmacro 0',\
-          os.path.join(thisDir,'json_timestep_to_txt.py'),
+          os.path.join(rootDir,'tests','json_timestep_to_txt.py'),
           #os.path.join(rootDir,'tests','timeout.sh') + ' -t 6 ' + os.path.join(rootDir,'py3','plot_spider.py') + ' -t 0',\
           ]
 
@@ -80,25 +65,20 @@ def blackbody_init_mixed(rootDir) :
       t.compareFloatingPointRelative(re.escape('scaling: '),1e-14)
       t.compareFloatingPointRelative(re.escape('val: '),    1e-5)
 
-  # Create Test Object
   t = pthtest.Test(testName,ranks,launch,expectedFile)
   t.setComparisonFile('out.txt')
   t.setVerifyMethod(comparefunc)
   t.setWalltime(1) # minutes
   t.setUseSandbox()
-
   return(t)
 
 def blackbody_init_solid(rootDir) :
   thisDir = os.path.split(os.path.abspath(__file__))[0]
   testName = "blackbody_init_solid"
   ranks = 1
-  # Run the test,
-  # then use a local script to generate a text file to compare with,
-  # then run the plotting script (output not checked)
   launch = [\
           os.path.join(rootDir,'spider')  + ' -options_file ' + os.path.join(rootDir,'examples','bower_2019','blackbody','bu_input.opts') + ' -ic_adiabat_entropy 1100' + ' -nstepsmacro 0',\
-          os.path.join(thisDir,'json_timestep_to_txt.py'),
+          os.path.join(rootDir,'tests','json_timestep_to_txt.py'),
           #os.path.join(rootDir,'tests','timeout.sh') + ' -t 6 ' + os.path.join(rootDir,'py3','plot_spider.py') + ' -t 0',\
           ]
 
@@ -114,5 +94,4 @@ def blackbody_init_solid(rootDir) :
   t.setVerifyMethod(comparefunc)
   t.setWalltime(1) # minutes
   t.setUseSandbox()
-
   return(t)
