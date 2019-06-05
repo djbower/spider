6c457510b563cfc68f81974e24f522d997142236
Dan J Bower <djbower@gmail.com>
2019-06-05 11:04:10 +0200

    more implementing event handling

--- poststep.c
+++ poststep.c
@@ -30,12 +30,17 @@ PetscErrorCode PostStep(TS ts)
   PetscInt       stepNumber;
   Ctx            *ctx;
   PostStepData   *data;
+  Parameters     *P;
   AtmosphereParameters *Ap;
+  Vec            X;
+  PetscScalar    x_CO2, x_H2O;
+  PetscInt       i;
 
   PetscFunctionBeginUser;
   ierr = TSGetTimeStepNumber(ts,&stepNumber);CHKERRQ(ierr);
   ierr = TSGetApplicationContext(ts,&ctx);CHKERRQ(ierr);
-  Ap = &ctx->parameters.atmosphere_parameters;
+  P = &ctx->parameters;
+  Ap = &P->atmosphere_parameters;
   if (!ctx->parameters.rollBackActive) SETERRQ(PetscObjectComm((PetscObject)ts),PETSC_ERR_SUP,"You must run with -activate_rollback");
   data = (PostStepData*) ctx->postStepData;
   //ierr = PetscPrintf(PetscObjectComm((PetscObject)ts),"PLACEHOLDER I'm %s in %s:%d, x_CO2 is %g\n",__func__,__FILE__,__LINE__,data->x_CO2);
@@ -43,7 +48,17 @@ PetscErrorCode PostStep(TS ts)
   //ierr = PetscPrintf(PetscObjectComm((PetscObject)ts),"PLACEHOLDER I'm %s in %s:%d, x_H2O is %g\n",__func__,__FILE__,__LINE__,data->x_H2O);
   // FIXME djbower add actual check (pull out current data with TSGetSolution(), use usual methods to access fields..)
 
-  if ((data->x_CO2 > 1.1 * Ap->CO2_parameters.initial) || (data->x_H2O > 1.1* Ap->H2O_parameters.initial)){
+  ierr = TSGetSolution(ts,&X);CHKERRQ(ierr);
+
+  // FIXME: better way to find the indices?
+  /* save current volatile concentrations in the melt */
+  i = P->numpts_b + 1;
+  ierr = VecGetValues(X,1,&i,&x_CO2);CHKERRQ(ierr);
+  i = P->numpts_b + 2;
+  ierr = VecGetValues(X,1,&i,&x_H2O);CHKERRQ(ierr);
+
+  //if ((data->x_CO2 > 1.1 * Ap->CO2_parameters.initial) || (data->x_H2O > 1.1* Ap->H2O_parameters.initial)){
+  if ((x_CO2 > 1.1*data->x_CO2) || (x_H2O > 1.1*data->x_H2O)){
     ierr = PetscPrintf(PetscObjectComm((PetscObject)ts),"PLACEHOLDER I'm %s in %s:%d, rolling back solution and setting flags\n",__func__,__FILE__,__LINE__);
     ierr = TSRollBack(ts);CHKERRQ(ierr);
     ierr = TSSetConvergedReason(ts,TS_CONVERGED_USER);CHKERRQ(ierr);
