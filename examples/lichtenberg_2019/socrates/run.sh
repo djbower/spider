55e263f330fde1810ab6b759e8f7e5fe3b172343
Dan J Bower <djbower@gmail.com>
2019-06-06 14:22:29 +0200

    more explanation of template case

--- examples/lichtenberg_2019/socrates/run.sh
+++ examples/lichtenberg_2019/socrates/run.sh
@@ -8,10 +8,11 @@
 # ======================
 # -options_file bu_input.opts:     reads the options file to provide the parameters for SPIDER
 #                                  subsequent options on the command line over-ride these defaults
-# -initial_condition 2:            read in ic from file
+# -initial_condition 2:            read in ic from file (prescribed by -ic_filename, see below)
 # -ic_filename 50001.json:         JSON file to read in initial condition
 # -SURFACE_BC 4:                   constant heat flux boundary condition
 # -surface_bc_value 1.0E4:         prescribed surface heat flux (e.g., 10^4 W/m^2)
+#                                  this value will be determined by SOCRATES and passed in
 # -SOLVE_FOR_VOLATILES 1:          track evolution of volatiles (CO2 and H2O) in interior/atmosphere reservoirs
 # -activate_rollback:              revert to previous timestep when an event has been triggered
 #                                  (see H2O_poststep_change and CO2_poststep_change below)
@@ -19,11 +20,12 @@
 # -H2O_poststep_change 0.05:       fractional change in H2O concentration in the melt phase that triggers an event
 #                                  (default here is 5% change, where the reference value is read in from the
 #                                  ic file)
-# -CO2_poststep_change 0.05:       as above for CO2
-# the options below effectively enable you to accommodate a maximum timestep before the code terminates, since
+# -CO2_poststep_change 0.05:       as above for CO2, also a 5% change to trigger an event
+# now, the options below effectively enable you to accommodate a maximum timestep before the code terminates, since
 # you may want to update atmospheric escape (or another quantity) within a given time frame even if an event
 # has not been triggered due to the change in volatile concentration.  Hence we are using a single time step
-# to provide another 'event' that halts the code
+# to provide another 'event' that halts the code.  In essence, dtmacro prescribes the maximum time (in years)
+# to advance before the code again halts even if an event has not occurred.
 # -nstepsmacro 1:                  make a single timestep
 # -dtmacro 30000:                  delta time to advance by, here is 30,000 years
 
