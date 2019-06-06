5de198e41d01fbbd36cea0b99d19a50022801f80
Dan J Bower <djbower@gmail.com>
2019-06-06 10:01:26 +0200

    description of options for event handling

--- examples/lichtenberg_2019/event/run.sh
+++ examples/lichtenberg_2019/event/run.sh
@@ -1,5 +1,30 @@
 #/user/bin/env bash
 
-# H2O_poststep_change and -CO2_poststep_change are the fractional changes in H2O and CO2 compared to the initialisation values, which are read in from the restart file.  Once these criteria are met, the code halts (activates event handling, effectively)
+# Template file for running SPIDER within the framework of a coupled interior/atmosphere/escape
+# scheme
+
+# ======================
+# Description of options
+# ======================
+# -options_file bu_input.opts:     reads the options file to provide the parameters for SPIDER
+#                                  subsequent options on the command line over-ride these defaults
+# -initial_condition 2:            read in ic from file
+# -ic_filename 0.json:             JSON file to read in initial condition
+# -SURFACE_BC 4:                   constant heat flux boundary condition
+# -surface_bc_value 1.0E4:         prescribed surface heat flux (e.g., 10^4 W/m^2)
+# -SOLVE_FOR_VOLATILES 1:          track evolution of volatiles (CO2 and H2O) in interior/atmosphere reservoirs
+# -activate_rollback:              revert to previous timestep when an event has been triggered
+#                                  (see H2O_poststep_change and CO2_poststep_change below)
+# -activate_poststep:              run a poststep function to determine if an event has occurred
+# -H2O_poststep_change 0.05:       fractional change in H2O concentration in the melt phase that triggers an event
+#                                  (default here is 5% change, where the reference value is read in from the
+#                                  ic file)
+# -CO2_poststep_change 0.05:       as above for CO2
+# the options below effectively enable you to accommodate a maximum timestep before the code terminates, since
+# you may want to update atmospheric escape (or another quantity) within a given time frame even if an event
+# has not been triggered due to the change in volatile concentration.  Hence we are using a single time step
+# to provide another 'event' that halts the code
+# -nstepsmacro 1:                  make a single timestep
+# -dtmacro 30000:                  delta time to advance by, here is 30,000 years
 
 spider -options_file bu_input.opts -initial_condition 2 -ic_filename 0.json -SURFACE_BC 4 -surface_bc_value 1.0E4 -SOLVE_FOR_VOLATILES 1 -activate_rollback -activate_poststep -H2O_poststep_change 0.05 -CO2_poststep_change 0.05 -nstepsmacro 1 -dtmacro 30000
