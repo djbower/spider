5721b2e1e800e0d4ab409081557e1e69e08b9d3f
Patrick Sanan <patrick.sanan@gmail.com>
2019-09-24 17:01:01 +0200

    Atmos tests: update output for reactions, and check more json files for "atmosphere"

--- tests/expected_output/tests.py
+++ tests/expected_output/tests.py
@@ -6,9 +6,11 @@ def atmosphere(rootDir) :
   thisDir = os.path.split(os.path.abspath(__file__))[0]
   testName = "atmosphere"
   ranks = 1
+  # Long range of acceptable files (probably better to check earlier)
+  acceptable_files = ' '.join(['output/'+str(i)+'.json' for i in range(1000000,1000124)])
   launch = [\
           os.path.join(rootDir,'spider')  + ' -options_file ' + os.path.join(rootDir,'examples','bower_2019','atmosphere','bu_input.opts'),\
-          os.path.join(rootDir,'tests','copy_one_of.py') + ' output/1000121.json output/1000122.json output/1000123.json -o to_check.json',\
+          os.path.join(rootDir,'tests','copy_one_of.py') + ' ' + acceptable_files + ' -o to_check.json',\
           os.path.join(rootDir,'tests','json_timestep_to_txt.py to_check.json'),
           #os.path.join(rootDir,'tests','timeout.sh') + ' -t 6 ' + os.path.join(rootDir,'py3','plot_spider.py') + ' -t 0,100,200,400,800,1200,1500',\
           ]
