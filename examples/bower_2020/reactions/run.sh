c252a069076d02679dd77de853b2359b2cd86e56
Dan J Bower <djbower@gmail.com>
2019-10-15 11:31:56 +0200

    more testing commands

--- examples/bower_2020/reactions/run.sh
+++ examples/bower_2020/reactions/run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+spider -options_file reactions.opt -atmosic_snes_verbose_monitor
+
 # sledgehammer
-#spider -options_file reactions.opt  -atmosic_snes_verbose_monitor -atmosic_snes_view -atmosic_ksp_rtol 1e-8 -atmosic_ksp_atol 1e-8  -atmosic_snes_converged_reason -atmosic_snes_linesearch_damping 0.01 -atmosic_snes_max_it 10000 -snes_linesearch_max_it 1000
+#spider -options_file reactions.opt  -atmosic_snes_verbose_monitor -atmosic_snes_view -atmosic_ksp_rtol 1e-10 -atmosic_ksp_atol 1e-10  -atmosic_snes_converged_reason -atmosic_snes_linesearch_damping 0.01 -atmosic_snes_max_it 10000 -snes_linesearch_max_it 1000
 
 # Dan's playing around with the below
-spider -options_file reactions.opt  -atmosic_snes_verbose_monitor -atmosic_snes_view  -atmosic_snes_converged_reason -atmosic_snes_atol 1.0E-6 -atmosic_snes_atol 1.0E-6 -atmosic_ksp_atol 1.0E-6 -atmosic_ksp_atol 1.0E-6 -atmosic_snes_max_it 10000 -snes_linesearch_max_it 1000
+#spider -options_file reactions.opt  -atmosic_snes_verbose_monitor -atmosic_snes_view  -atmosic_snes_converged_reason -atmosic_snes_atol 1.0E-6 -atmosic_snes_atol 1.0E-6 -atmosic_ksp_atol 1.0E-6 -atmosic_ksp_atol 1.0E-6 -atmosic_snes_max_it 10000 -snes_linesearch_max_it 1000
