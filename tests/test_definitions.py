539016dcab3a1ced4eb9b196c34a97fc66f47d39
Patrick Sanan <patrick.sanan@gmail.com>
2019-11-07 09:32:04 +0100

    Tests: reorganization and cleanup
    * tests.py: remove old comments
    * rename tests/expected_output/tests.py to tests/test_definitions.py
    * rename examples/unit_tests/ to examples/tests/ (these aren't unit tests, but application tests)

--- tests/test_definitions.py
+++ tests/test_definitions.py
@@ -9,13 +9,12 @@ def atmosphere(rootDir) :
   # Long range of acceptable files (probably better to check earlier)
   acceptable_files = ' '.join(['output/'+str(i)+'.json' for i in range(1000000,1000124)])
   launch = [\
-          os.path.join(rootDir,'spider')  + ' -options_file ' + os.path.join(rootDir,'examples','unit_tests','atmosphere','bu_input.opts'),\
+          os.path.join(rootDir,'spider')  + ' -options_file ' + os.path.join(rootDir,'examples','tests','atmosphere','bu_input.opts'),\
           os.path.join(rootDir,'tests','copy_one_of.py') + ' ' + acceptable_files + ' -o to_check.json',\
           os.path.join(rootDir,'tests','json_timestep_to_txt.py to_check.json'),
-          #os.path.join(rootDir,'tests','timeout.sh') + ' -t 6 ' + os.path.join(rootDir,'py3','plot_spider.py') + ' -t 0,100,200,400,800,1200,1500',\
           ]
 
-  expectedFile = os.path.join(thisDir,'expected_atmosphere.txt')
+  expectedFile = os.path.join(thisDir,'expected_output','expected_atmosphere.txt')
 
   def comparefunc(t) :
       t.compareFloatingPointRelative(re.escape('scaling: '),1e-14)
@@ -35,13 +34,12 @@ def atmosphere_jeans(rootDir) :
   # Long range of acceptable files (probably better to check earlier)
   acceptable_files = ' '.join(['output/'+str(i)+'.json' for i in range(1000000,1000097)])
   launch = [\
-          os.path.join(rootDir,'spider')  + ' -options_file ' + os.path.join(rootDir,'examples','unit_tests','atmosphere_jeans','bu_input.opts'),\
+          os.path.join(rootDir,'spider')  + ' -options_file ' + os.path.join(rootDir,'examples','tests','atmosphere_jeans','bu_input.opts'),\
           os.path.join(rootDir,'tests','copy_one_of.py') + ' ' + acceptable_files + ' -o to_check.json',\
           os.path.join(rootDir,'tests','json_timestep_to_txt.py to_check.json'),
-          #os.path.join(rootDir,'tests','timeout.sh') + ' -t 6 ' + os.path.join(rootDir,'py3','plot_spider.py') + ' -t 0,100,200,400,800,1200,1500',\
           ]
 
-  expectedFile = os.path.join(thisDir,'expected_atmosphere_jeans.txt')
+  expectedFile = os.path.join(thisDir,'expected_output','expected_atmosphere_jeans.txt')
 
   def comparefunc(t) :
       t.compareFloatingPointRelative(re.escape('scaling: '),1e-14)
@@ -59,12 +57,11 @@ def blackbody(rootDir) :
   testName = "blackbody"
   ranks = 1
   launch = [\
-          os.path.join(rootDir,'spider')  + ' -options_file ' + os.path.join(rootDir,'examples','unit_tests','blackbody','bu_input.opts'),\
+          os.path.join(rootDir,'spider')  + ' -options_file ' + os.path.join(rootDir,'examples','tests','blackbody','bu_input.opts'),\
           os.path.join(rootDir,'tests','json_timestep_to_txt.py output/100.json'),
-          #os.path.join(rootDir,'tests','timeout.sh') + ' -t 6 ' + os.path.join(rootDir,'py3','plot_spider.py') + ' -t 0,100,200,400,800,1200,1500',\
           ]
 
-  expectedFile = os.path.join(thisDir,'expected_blackbody.txt')
+  expectedFile = os.path.join(thisDir,'expected_output','expected_blackbody.txt')
 
   def comparefunc(t) :
       t.compareFloatingPointRelative(re.escape('scaling: '),1e-14)
@@ -82,12 +79,11 @@ def blackbody_init_liquid(rootDir) :
   testName = "blackbody_init_liquid"
   ranks = 1
   launch = [\
-          os.path.join(rootDir,'spider')  + ' -options_file ' + os.path.join(rootDir,'examples','unit_tests','blackbody','bu_input.opts') + ' -ic_adiabat_entropy 2700' + ' -nstepsmacro 0',\
+          os.path.join(rootDir,'spider')  + ' -options_file ' + os.path.join(rootDir,'examples','tests','blackbody','bu_input.opts') + ' -ic_adiabat_entropy 2700' + ' -nstepsmacro 0',\
           os.path.join(rootDir,'tests','json_timestep_to_txt.py'),
-          #os.path.join(rootDir,'tests','timeout.sh') + ' -t 6 ' + os.path.join(rootDir,'py3','plot_spider.py') + ' -t 0',\
           ]
 
-  expectedFile = os.path.join(thisDir,'expected_blackbody_init_liquid.txt')
+  expectedFile = os.path.join(thisDir,'expected_output','expected_blackbody_init_liquid.txt')
 
   def comparefunc(t) :
       t.compareFloatingPointRelative(re.escape('scaling: '),1e-14)
@@ -106,12 +102,11 @@ def blackbody_init_mixed(rootDir) :
   testName = "blackbody_init_mixed"
   ranks = 1
   launch = [\
-          os.path.join(rootDir,'spider')  + ' -options_file ' + os.path.join(rootDir,'examples','unit_tests','blackbody','bu_input.opts') + ' -ic_adiabat_entropy 1900' + ' -nstepsmacro 0',\
+          os.path.join(rootDir,'spider')  + ' -options_file ' + os.path.join(rootDir,'examples','tests','blackbody','bu_input.opts') + ' -ic_adiabat_entropy 1900' + ' -nstepsmacro 0',\
           os.path.join(rootDir,'tests','json_timestep_to_txt.py'),
-          #os.path.join(rootDir,'tests','timeout.sh') + ' -t 6 ' + os.path.join(rootDir,'py3','plot_spider.py') + ' -t 0',\
           ]
 
-  expectedFile = os.path.join(thisDir,'expected_blackbody_init_mixed.txt')
+  expectedFile = os.path.join(thisDir,'expected_output','expected_blackbody_init_mixed.txt')
 
   def comparefunc(t) :
       t.compareFloatingPointRelative(re.escape('scaling: '),1e-14)
@@ -129,16 +124,15 @@ def blackbody_init_solid(rootDir) :
   testName = "blackbody_init_solid"
   ranks = 1
   launch = [\
-          os.path.join(rootDir,'spider')  + ' -options_file ' + os.path.join(rootDir,'examples','unit_tests','blackbody','bu_input.opts') + ' -ic_adiabat_entropy 1100' + ' -nstepsmacro 0',\
+          os.path.join(rootDir,'spider')  + ' -options_file ' + os.path.join(rootDir,'examples','tests','blackbody','bu_input.opts') + ' -ic_adiabat_entropy 1100' + ' -nstepsmacro 0',\
           os.path.join(rootDir,'tests','json_timestep_to_txt.py'),
-          #os.path.join(rootDir,'tests','timeout.sh') + ' -t 6 ' + os.path.join(rootDir,'py3','plot_spider.py') + ' -t 0',\
           ]
 
-  expectedFile = os.path.join(thisDir,'expected_blackbody_init_solid.txt')
+  expectedFile = os.path.join(thisDir,'expected_output','expected_blackbody_init_solid.txt')
 
   def comparefunc(t) :
       t.compareFloatingPointRelative(re.escape('scaling: '),1e-14)
-      t.compareFloatingPointRelative(re.escape('val: '),    1e-10) # likely too tight
+      t.compareFloatingPointRelative(re.escape('val: '),    1e-10)
 
   # Create Test Object
   t = pthtest.Test(testName,ranks,launch,expectedFile)
