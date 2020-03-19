acdd63b9d58b74499dcd38eb1b5b34925a9c79e0
Dan Bower <djbower@gmail.com>
2020-03-19 20:05:50 +0100

    update restart examples

--- examples/lichtenberg_2019/socrates/run.sh
+++ examples/lichtenberg_2019/socrates/run.sh
@@ -9,9 +9,9 @@
 # -options_file bu_input.opts:     reads the options file to provide the parameters for SPIDER
 #                                  subsequent options on the command line over-ride these defaults
 # -IC_INTERIOR 2:                  read in interior ic from file (prescribed by -ic_interior_filename, see below)
-# -ic_interior_filename 50008.json:JSON file to read in interior initial condition
+# -ic_interior_filename 50013.json:JSON file to read in interior initial condition
 # -IC_ATMOSPHERE 2:                read in atmosphere ic from file (prescribed by -ic_atmosphere_filename, see below)
-# -ic_atmosphere_filename 50008.json:JSON file to read in atmosphere initial condition
+# -ic_atmosphere_filename 50013.json:JSON file to read in atmosphere initial condition
 # -SURFACE_BC 4:                   constant heat flux boundary condition
 # -surface_bc_value 1.0E4:         prescribed surface heat flux (e.g., 10^4 W/m^2)
 #                                  this value will be determined by SOCRATES and passed in
@@ -36,11 +36,11 @@
 # Overview of result
 # ==================
 # TODO: Dan has not updated the following in light of the tsurf_poststep_change criteria!
-# data is read from 50008.json, and the time-stepper runs until the H2O partial pressure tration in the mantle exceed a
-# 5% change.  Two files are output in the output/ directory.  The first, output/50008.json is the first output
-# that reflects the new state of the system computed from the restart data. The second file output is 56710.json.
+# data is read from 50013.json, and the time-stepper runs until the H2O partial pressure tration in the mantle exceed a
+# 5% change.  Two files are output in the output/ directory.  The first, output/50013.json is the first output
+# that reflects the new state of the system computed from the restart data. The second file output is 56714.json.
 # This output is triggered by the fact that H2O partial pressure now exceeds a 5% change, relative to the value
-# that was read in from 50008.json (you can easily see this by manually checking the values in the JSONs).
+# that was read in from 50013.json (you can easily see this by manually checking the values in the JSONs).
 
 # Now, the coupler could either search for the last time output, and use this to restart, or we could generate
 # a generically named restart file which is a clone of the last output.  Basically, this would allow us to have
@@ -49,8 +49,8 @@
 # SPIDER to do it.
 
 # generate the restart file from the initial condition
-# this will output 50008.json in output/
+# this will output 50013.json in output/
 #spider -options_file bu_input.opts
 
 # main restart example is here:
-spider -options_file bu_input.opts -IC_INTERIOR 2 -ic_interior_filename 50008.json -SURFACE_BC 4 -surface_bc_value 1.0E4 -IC_ATMOSPHERE 2 -ic_atmosphere_filename 50008.json -activate_rollback -activate_poststep -H2O_poststep_change 0.05 -CO2_poststep_change 0.05 -nstepsmacro 1 -dtmacro 30000
+spider -options_file bu_input.opts -IC_INTERIOR 2 -ic_interior_filename 50013.json -SURFACE_BC 4 -surface_bc_value 1.0E4 -IC_ATMOSPHERE 2 -ic_atmosphere_filename 50013.json -activate_rollback -activate_poststep -H2O_poststep_change 0.05 -CO2_poststep_change 0.05 -nstepsmacro 1 -dtmacro 30000
