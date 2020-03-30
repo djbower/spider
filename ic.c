60d0750651e992c667dc11ad29805e78ee05aa02
Dan Bower <djbower@gmail.com>
2020-03-30 09:16:12 +0200

    fix uninitialised value warning (valgrind) when no volatiles are present

--- ic.c
+++ ic.c
@@ -533,7 +533,10 @@ static PetscErrorCode print_ocean_masses( Ctx *E )
     PetscInt i;
     PetscScalar mass_H2, mass_H2O, molar_mass_H2, molar_mass_H2O;
     PetscScalar mass_CO, mass_CO2, molar_mass_CO, molar_mass_CO2;
-    PetscBool   FLAG_H2, FLAG_H2O, FLAG_CO, FLAG_CO2;
+    PetscBool   FLAG_H2 = PETSC_FALSE;
+    PetscBool   FLAG_H2O = PETSC_FALSE; 
+    PetscBool   FLAG_CO = PETSC_FALSE;
+    PetscBool   FLAG_CO2 = PETSC_FALSE;
     PetscScalar tmass_H2, tmass_H2O, p_H2, p_H2O;
     PetscScalar tmass_CO, tmass_CO2, p_CO, p_CO2;
 
