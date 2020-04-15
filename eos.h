b28e9bb6a201f26e3b9ed29d59d5bf9e3f269715
Dan Bower <djbower@gmail.com>
2020-04-15 10:12:13 +0200

    rename functions and some preparation for a generic interface for EOS evaluation

--- eos.h
+++ eos.h
@@ -11,9 +11,12 @@ PetscErrorCode EosParametersSetFromOptions( EosParameters, const FundamentalCons
 /* TODO: below probably moves elsewhere eventually (becomes static?) */
 PetscErrorCode set_rtpress_struct( PetscScalar, PetscScalar, Ctx * );
 
+#if 0
+/* TODO: need to refresh with new eos evaluation structs */
 /* for function testing */
 PetscScalar get_rtpress_pressure_test( Ctx * );
 PetscScalar get_rtpress_entropy_test( Ctx * );
+#endif
 
 PetscScalar get_val1d( Interp1d const, PetscScalar );
 PetscScalar get_val2d( Interp2d const, PetscScalar, PetscScalar );
