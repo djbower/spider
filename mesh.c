4bc16c6b87c89225829abb49cacdf92b23119a84
Dan Bower <djbower@gmail.com>
2020-09-03 19:51:58 +0200

    one function for refining the uppermost mesh and another for refining the lowermost

--- mesh.c
+++ mesh.c
@@ -1,7 +1,8 @@
 #include "mesh.h"
 
 static PetscErrorCode regular_mesh( Ctx * );
-static PetscErrorCode geometric_mesh( Ctx * );
+static PetscErrorCode geometric_mesh_refine_upper( Ctx * );
+static PetscErrorCode geometric_mesh_refine_lower( Ctx * );
 static PetscErrorCode spherical_area( DM, Vec, Vec );
 static PetscErrorCode spherical_volume( Ctx *, Vec, Vec );
 // FIXME: TODO: REMOVE
@@ -29,8 +30,12 @@ PetscErrorCode set_mesh( Ctx *E)
 
     /* need to use geometric mesh to resolve ultra-thin thermal
        boundary layer at the base of the mantle */
+#if 0
+    geometric_mesh_refine_lower( E );
+#endif
+
 #if 1
-    geometric_mesh( E );
+    geometric_mesh_refine_upper( E );
 #endif
 
     /* Adams-Williamson EOS */
@@ -115,8 +120,95 @@ static PetscErrorCode regular_mesh( Ctx *E )
 
 }
 
+
 #if 1
-static PetscErrorCode geometric_mesh( Ctx *E )
+static PetscErrorCode geometric_mesh_refine_upper( Ctx *E )
+{
+
+    PetscErrorCode ierr;
+    PetscScalar    *arr_b, *arr_s;
+    PetscInt       i,ilo_b,ihi_b,ilo_s,ihi_s,w_b,w_s,numpts_b,numpts_s;
+    Mesh           *M;
+    DM             da_b=E->da_b, da_s=E->da_s;
+    Parameters     P = E->parameters;
+    ScalingConstants SC = E->parameters->scaling_constants;
+    PetscScalar    dr, dr_prev, dr_min, dr_max, geom_fac;
+
+    /* TODO: you must set the number of nodes manually in the opts file
+       according to the values below (and they vary depending on the choice
+       of dr_max */
+    dr_min = 1.0E-3; // 1 mm
+    //dr_max = 15E3; // 15 km
+    dr_max = 5E3; // 1 km
+    geom_fac = 1.2;
+
+    dr_min /= SC->RADIUS;
+    dr_max /= SC->RADIUS;
+
+    // 45928 basic nodes
+    //PetscScalar dr_max = 9.810076911002982e-06; // 62.5 m
+    // 22996 basic nodes
+    //PetscScalar dr_max = 1.9620153822005963e-05; // 125 m
+    // 11532 basic nodes
+    //PetscScalar dr_max = 3.924030764401193e-05; // 250 m
+    // 5802 basic nodes
+    //PetscScalar dr_max = 7.848061528802385e-05; // 500 m
+    // 2939 basic nodes
+    //PetscScalar    dr_max = 0.0001569612305760477; // 1 km
+    // 656 basic nodes (655 for upper refine)
+    //PetscScalar    dr_max = 0.0007848061528802385; // 5 km
+    // 372 basic nodes
+    //PetscScalar    dr_max = 0.001569612305760477; // 10 km
+    // 278 basic nodes
+    // PetscScalar    dr_max = 15E3 / SC->RADIUS; // 15 km, dimensional
+
+    PetscFunctionBeginUser;
+
+    M = &E->mesh;
+
+    ierr = DMDAGetInfo(E->da_b,NULL,&numpts_b,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);CHKERRQ(ierr);
+    ierr = DMDAGetInfo(E->da_s,NULL,&numpts_s,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);CHKERRQ(ierr);
+
+    /* TODO: dynamically determine number of mesh points, and
+       automatically ensure that arr[0] = 1.0 and
+       arr[numpts_b-1] = RADIN */
+
+    /* radius at basic nodes */
+    ierr = DMDAGetCorners(da_b,&ilo_b,0,0,&w_b,0,0);CHKERRQ(ierr);
+    ihi_b = ilo_b + w_b;
+    ierr = DMDAVecGetArray(da_b,M->radius_b,&arr_b);CHKERRQ(ierr);
+    arr_b[0] =  P->radius;
+    dr_prev = dr_min / geom_fac;
+    for (i=ilo_b+1; i<ihi_b; ++i){ /* Note upper bound */
+        dr = dr_prev * geom_fac;
+        if( dr > dr_max){
+            dr = dr_max;
+        }
+        dr_prev = dr;
+        arr_b[i] = arr_b[i-1] - dr;
+    }
+    // TODO: below is hacky.
+    arr_b[numpts_b-1] = P->radius * P->coresize;
+
+    ierr = DMDAVecRestoreArray(da_b,M->radius_b,&arr_b);CHKERRQ(ierr);
+
+    /* radius at staggered nodes */
+    ierr = DMDAGetCorners(da_s,&ilo_s,0,0,&w_s,0,0);CHKERRQ(ierr);
+    ihi_s = ilo_s + w_s;
+    ierr = DMDAVecGetArray(da_s,M->radius_s,&arr_s);CHKERRQ(ierr);
+    for (i=ilo_s;i<ihi_s;++i){
+        arr_s[i] = 0.5 * (arr_b[i]+arr_b[i+1]);
+    }
+    ierr = DMDAVecRestoreArray(da_s,M->radius_s,&arr_s);CHKERRQ(ierr);
+
+    PetscFunctionReturn(0);
+
+}
+#endif
+
+
+#if 1
+static PetscErrorCode geometric_mesh_refine_lower( Ctx *E )
 {
 
     PetscErrorCode ierr;
