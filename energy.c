35d4fd8e117b78bc26c4fc76c67803062db70dc4
Dan Bower <djbower@gmail.com>
2020-10-05 11:29:17 +0200

    cleaning up comments

--- energy.c
+++ energy.c
@@ -506,7 +506,6 @@ static PetscErrorCode objective_function_surfacebc( SNES snes, Vec x, Vec f, voi
     /* get first staggered node value (store as Ss0) */
     ierr = VecGetValues(S->S_s,1,&ind0,&Ss0);CHKERRQ(ierr);
     /* surface mapping from mass coordinate to radius */
-    /* TODO: check not zero or NaN */
     ierr = VecGetValues(M->dxidr_b,1,&ind0,&dxidr0);CHKERRQ(ierr);
     ierr = VecGetValues(M->radius_b,1,&ind0,&radius0);CHKERRQ(ierr);
 
@@ -514,8 +513,6 @@ static PetscErrorCode objective_function_surfacebc( SNES snes, Vec x, Vec f, voi
        entropy (uppermost basic node) using our reconstruction */
     Sb0 = -dSdxi0 * 0.5 * (arr_xi_b[1] - arr_xi_b[0]) + Ss0;
     
-    /* now process dSdxi0 and other arguments to get the residual of the surface boundary
-       condition */
     /* need material properties at this entropy and surface pressure (0 GPa).  Since these
        are lookup quantities it precludes defining a Jacobian */
     ierr = EOSEval( P->eos, 0.0, Sb0, &eos_eval );CHKERRQ(ierr);
@@ -526,24 +523,15 @@ static PetscErrorCode objective_function_surfacebc( SNES snes, Vec x, Vec f, voi
     /* radiative flux */
     res = emissivity * FC->STEFAN_BOLTZMANN * ( PetscPowScalar( eos_eval.T, 4.0 ) - PetscPowScalar( Ap->teqm, 4.0 ) );
     
-    /* conductive flux */
-    /* TODO: does arr_dPdr_b[i] have a valid value at i=0? */ 
-    /* Jcond is negaative, so this is positive for residual calculation */
+    /* conductive flux (negative by definition, so positive for residual) */
     res += eos_eval.cond * (eos_eval.T / eos_eval.Cp * dSdxi0 * dxidr0 + arr_dPdr_b[ind0] * eos_eval.dTdPs);
   
-    /* for normalising residual? */ 
-    //res /= emissivity * FC->STEFAN_BOLTZMANN * ( PetscPowScalar( eos_eval.T, 4.0 ) - PetscPowScalar( Ap->teqm, 4.0 ) ); 
-    //res *= 1.0E8;
-    //res = res - 1.0;
-
-    /* convective flux, probably only works when mixing length constant */
-    /* FIXME TODO */
-
+    /* convective flux (negative by definition, so positive for residual) */
     PetscScalar kappah;
     ierr = GetEddyDiffusivity( eos_eval, P, radius0, dSdxi0, dxidr0, &kappah, NULL, NULL );CHKERRQ(ierr);
-
     res += dSdxi0 * dxidr0 * kappah * eos_eval.rho * eos_eval.T;
 
+    /* set residual of fluxes */
     ff[ind0] = res;
     
     ierr = VecRestoreArrayRead(x,&xx);CHKERRQ(ierr);
