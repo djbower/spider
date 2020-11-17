djbower/spider

commit c85721348b085e390448ae2d8cfb5a3e95afece0

mixing length was being set to zero at the base of the mantle due to tiny numerical differences between the radius and coresize.  Fix that here.  Changes nothing for previous SPIDER versions, but now necessary to fix to include new boundary conditions.

parameters.c
@@ -297,11 +297,16 @@ PetscErrorCode ParametersSetFromOptions(Parameters P)
 
   /* option to include a mid-mantle layer
      this is non-dimensional fractional radius, as with coresize
-     if set to P->coresize, then a single layer is recovered (default)
+     Default of 1.0E-10 is effectively zero so gives a single layer
      note this only influences the radial mixing length (not viscosity etc.) */
-  ierr = PetscOptionsGetPositiveScalar("-layer_interface_radius",&P->layer_interface_radius,P->coresize,NULL);CHKERRQ(ierr);
-  if( (P->layer_interface_radius < P->coresize || P->layer_interface_radius > 1.0) ){
-      SETERRQ1(PETSC_COMM_WORLD,PETSC_ERR_ARG_OUTOFRANGE,"-layer_interface_radius %f must be greater than -coresize and less than 1.0",P->layer_interface_radius);
+  {
+    PetscBool set;
+    ierr = PetscOptionsGetPositiveScalar("-layer_interface_radius",&P->layer_interface_radius,1.0E-10,&set);CHKERRQ(ierr);
+    if(set){
+      if( (P->layer_interface_radius < P->coresize || P->layer_interface_radius > 1.0) ){
+        SETERRQ1(PETSC_COMM_WORLD,PETSC_ERR_ARG_OUTOFRANGE,"-layer_interface_radius %f must be greater than -coresize and less than 1.0",P->layer_interface_radius);
+      }
+    }
   }
 
   P->Mg_Si0 = 0.0;

commit 8722c336ae1d09879052060efa75f08fdf3632c7

reverse part of c85721348b085e390448ae2d8cfb5a3e95afece0 which introduced a bug.  WIP.

parameters.c
@@ -297,11 +297,10 @@ PetscErrorCode ParametersSetFromOptions(Parameters P)
 
   /* option to include a mid-mantle layer
      this is non-dimensional fractional radius, as with coresize
-     Default of 1.0E-10 is effectively zero so gives a single layer
      note this only influences the radial mixing length (not viscosity etc.) */
   {
     PetscBool set;
-    ierr = PetscOptionsGetPositiveScalar("-layer_interface_radius",&P->layer_interface_radius,1.0E-10,&set);CHKERRQ(ierr);
+    ierr = PetscOptionsGetPositiveScalar("-layer_interface_radius",&P->layer_interface_radius,P->coresize,&set);CHKERRQ(ierr);
     if(set){
       if( (P->layer_interface_radius < P->coresize || P->layer_interface_radius > 1.0) ){
         SETERRQ1(PETSC_COMM_WORLD,PETSC_ERR_ARG_OUTOFRANGE,"-layer_interface_radius %f must be greater than -coresize and less than 1.0",P->layer_interface_radius);

util.c
@@ -697,7 +697,7 @@ static PetscScalar GetMixingLength( const Parameters P, PetscScalar radius )
        enabling this single expression to work for both a single and double
        layered mantle */
 
-    if( radius > P->radius * P->layer_interface_radius ){
+    if( radius >= P->radius * P->layer_interface_radius ){
         outer_radius = P->radius;
         inner_radius = P->radius * P->layer_interface_radius;
     }   