5893fd68b404cb961d8ab4b99b5e968c7985a935
Dan J Bower <djbower@users.noreply.github.com>
2023-07-06 08:49:40 +0200

    Linter formatting

--- dev/mesh.c
+++ dev/mesh.c
@@ -1,25 +1,25 @@
-static PetscErrorCode SetMeshGeometricRefineUpper( Ctx * );
-static PetscErrorCode SetMeshGeometricRefineLower( Ctx * );
+static PetscErrorCode SetMeshGeometricRefineUpper(Ctx *);
+static PetscErrorCode SetMeshGeometricRefineLower(Ctx *);
 
-static PetscErrorCode SetMeshGeometricRefineUpper( Ctx *E )
+static PetscErrorCode SetMeshGeometricRefineUpper(Ctx *E)
 {
 
     PetscErrorCode ierr;
-    PetscScalar    *arr_b, *arr_s;
-    PetscInt       i,ilo_b,ihi_b,ilo_s,ihi_s,w_b,w_s,numpts_b,numpts_s;
-    Mesh           *M;
-    DM             da_b=E->da_b, da_s=E->da_s;
-    Parameters     P = E->parameters;
+    PetscScalar *arr_b, *arr_s;
+    PetscInt i, ilo_b, ihi_b, ilo_s, ihi_s, w_b, w_s, numpts_b, numpts_s;
+    Mesh *M;
+    DM da_b = E->da_b, da_s = E->da_s;
+    Parameters P = E->parameters;
     ScalingConstants SC = E->parameters->scaling_constants;
-    PetscScalar    dr, dr_prev, dr_min, dr_max, geom_fac;
+    PetscScalar dr, dr_prev, dr_min, dr_max, geom_fac;
 
     /* TODO: you must set the number of nodes manually in the opts file
        according to the values below (and they vary depending on the choice
        of dr_max */
-    //dr_min = 1.0E-3; // 1 mm
-    // n=959 for 1E3 to 3E3
+    // dr_min = 1.0E-3; // 1 mm
+    //  n=959 for 1E3 to 3E3
     dr_min = 1.0E3; // 10 m
-    //dr_max = 15E3; // 15 km
+    // dr_max = 15E3; // 15 km
     dr_max = 3.0E3; // 1 km
     geom_fac = 1.2;
 
@@ -27,19 +27,19 @@ static PetscErrorCode SetMeshGeometricRefineUpper( Ctx *E )
     dr_max /= SC->RADIUS;
 
     // 45928 basic nodes
-    //PetscScalar dr_max = 9.810076911002982e-06; // 62.5 m
+    // PetscScalar dr_max = 9.810076911002982e-06; // 62.5 m
     // 22996 basic nodes
-    //PetscScalar dr_max = 1.9620153822005963e-05; // 125 m
+    // PetscScalar dr_max = 1.9620153822005963e-05; // 125 m
     // 11532 basic nodes
-    //PetscScalar dr_max = 3.924030764401193e-05; // 250 m
+    // PetscScalar dr_max = 3.924030764401193e-05; // 250 m
     // 5802 basic nodes
-    //PetscScalar dr_max = 7.848061528802385e-05; // 500 m
+    // PetscScalar dr_max = 7.848061528802385e-05; // 500 m
     // 2939 basic nodes
-    //PetscScalar    dr_max = 0.0001569612305760477; // 1 km
+    // PetscScalar    dr_max = 0.0001569612305760477; // 1 km
     // 656 basic nodes (655 for upper refine)
-    //PetscScalar    dr_max = 0.0007848061528802385; // 5 km
+    // PetscScalar    dr_max = 0.0007848061528802385; // 5 km
     // 372 basic nodes
-    //PetscScalar    dr_max = 0.001569612305760477; // 10 km
+    // PetscScalar    dr_max = 0.001569612305760477; // 10 km
     // 278 basic nodes
     // PetscScalar    dr_max = 15E3 / SC->RADIUS; // 15 km, dimensional
 
@@ -47,81 +47,91 @@ static PetscErrorCode SetMeshGeometricRefineUpper( Ctx *E )
 
     M = &E->mesh;
 
-    ierr = DMDAGetInfo(E->da_b,NULL,&numpts_b,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);CHKERRQ(ierr);
-    ierr = DMDAGetInfo(E->da_s,NULL,&numpts_s,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);CHKERRQ(ierr);
+    ierr = DMDAGetInfo(E->da_b, NULL, &numpts_b, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+    CHKERRQ(ierr);
+    ierr = DMDAGetInfo(E->da_s, NULL, &numpts_s, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+    CHKERRQ(ierr);
 
     /* TODO: dynamically determine number of mesh points, and
        automatically ensure that arr[0] = radius and
        arr[numpts_b-1] = radius*P->coresize */
 
     /* radius at basic nodes */
-    ierr = DMDAGetCorners(da_b,&ilo_b,0,0,&w_b,0,0);CHKERRQ(ierr);
+    ierr = DMDAGetCorners(da_b, &ilo_b, 0, 0, &w_b, 0, 0);
+    CHKERRQ(ierr);
     ihi_b = ilo_b + w_b;
-    ierr = DMDAVecGetArray(da_b,M->xi_b,&arr_b);CHKERRQ(ierr);
-    arr_b[0] =  P->radius;
+    ierr = DMDAVecGetArray(da_b, M->xi_b, &arr_b);
+    CHKERRQ(ierr);
+    arr_b[0] = P->radius;
     dr_prev = dr_min / geom_fac;
-    for (i=ilo_b+1; i<ihi_b; ++i){ /* Note upper bound */
+    for (i = ilo_b + 1; i < ihi_b; ++i)
+    { /* Note upper bound */
         dr = dr_prev * geom_fac;
-        if( dr > dr_max){
+        if (dr > dr_max)
+        {
             dr = dr_max;
         }
         dr_prev = dr;
-        arr_b[i] = arr_b[i-1] - dr;
+        arr_b[i] = arr_b[i - 1] - dr;
     }
     // TODO: below is hacky.
-    arr_b[numpts_b-1] = P->radius * P->coresize;
+    arr_b[numpts_b - 1] = P->radius * P->coresize;
 
-    ierr = DMDAVecRestoreArray(da_b,M->xi_b,&arr_b);CHKERRQ(ierr);
+    ierr = DMDAVecRestoreArray(da_b, M->xi_b, &arr_b);
+    CHKERRQ(ierr);
 
     /* radius at staggered nodes */
-    ierr = DMDAGetCorners(da_s,&ilo_s,0,0,&w_s,0,0);CHKERRQ(ierr);
+    ierr = DMDAGetCorners(da_s, &ilo_s, 0, 0, &w_s, 0, 0);
+    CHKERRQ(ierr);
     ihi_s = ilo_s + w_s;
-    ierr = DMDAVecGetArray(da_s,M->xi_s,&arr_s);CHKERRQ(ierr);
-    for (i=ilo_s;i<ihi_s;++i){
-        arr_s[i] = 0.5 * (arr_b[i]+arr_b[i+1]);
+    ierr = DMDAVecGetArray(da_s, M->xi_s, &arr_s);
+    CHKERRQ(ierr);
+    for (i = ilo_s; i < ihi_s; ++i)
+    {
+        arr_s[i] = 0.5 * (arr_b[i] + arr_b[i + 1]);
     }
-    ierr = DMDAVecRestoreArray(da_s,M->xi_s,&arr_s);CHKERRQ(ierr);
+    ierr = DMDAVecRestoreArray(da_s, M->xi_s, &arr_s);
+    CHKERRQ(ierr);
 
     PetscFunctionReturn(0);
-
 }
 
-static PetscErrorCode SetMeshGeometricRefineLower( Ctx *E )
+static PetscErrorCode SetMeshGeometricRefineLower(Ctx *E)
 {
 
     PetscErrorCode ierr;
-    PetscScalar    *arr_b, *arr_s;
-    PetscInt       i,ilo_b,ihi_b,ilo_s,ihi_s,w_b,w_s,numpts_b,numpts_s;
-    Mesh           *M;
-    DM             da_b=E->da_b, da_s=E->da_s;
-    Parameters     P = E->parameters;
+    PetscScalar *arr_b, *arr_s;
+    PetscInt i, ilo_b, ihi_b, ilo_s, ihi_s, w_b, w_s, numpts_b, numpts_s;
+    Mesh *M;
+    DM da_b = E->da_b, da_s = E->da_s;
+    Parameters P = E->parameters;
     ScalingConstants SC = E->parameters->scaling_constants;
-    PetscScalar    dr, dr_prev, dr_min, dr_max, geom_fac;
+    PetscScalar dr, dr_prev, dr_min, dr_max, geom_fac;
 
     /* TODO: you must set the number of nodes manually in the opts file
        according to the values below (and they vary depending on the choice
        of dr_max */
     dr_min = 1.0E-3; // 1 mm
-    dr_max = 15E3; // 15 km
+    dr_max = 15E3;   // 15 km
     geom_fac = 1.2;
 
     dr_min /= SC->RADIUS;
     dr_max /= SC->RADIUS;
 
     // 45928 basic nodes
-    //PetscScalar dr_max = 9.810076911002982e-06; // 62.5 m
+    // PetscScalar dr_max = 9.810076911002982e-06; // 62.5 m
     // 22996 basic nodes
-    //PetscScalar dr_max = 1.9620153822005963e-05; // 125 m
+    // PetscScalar dr_max = 1.9620153822005963e-05; // 125 m
     // 11532 basic nodes
-    //PetscScalar dr_max = 3.924030764401193e-05; // 250 m
+    // PetscScalar dr_max = 3.924030764401193e-05; // 250 m
     // 5802 basic nodes
-    //PetscScalar dr_max = 7.848061528802385e-05; // 500 m
+    // PetscScalar dr_max = 7.848061528802385e-05; // 500 m
     // 2939 basic nodes
-    //PetscScalar    dr_max = 0.0001569612305760477; // 1 km
+    // PetscScalar    dr_max = 0.0001569612305760477; // 1 km
     // 656 basic nodes
-    //PetscScalar    dr_max = 0.0007848061528802385; // 5 km
+    // PetscScalar    dr_max = 0.0007848061528802385; // 5 km
     // 372 basic nodes
-    //PetscScalar    dr_max = 0.001569612305760477; // 10 km
+    // PetscScalar    dr_max = 0.001569612305760477; // 10 km
     // 278 basic nodes
     // PetscScalar    dr_max = 15E3 / SC->RADIUS; // 15 km, dimensional
 
@@ -129,43 +139,53 @@ static PetscErrorCode SetMeshGeometricRefineLower( Ctx *E )
 
     M = &E->mesh;
 
-    ierr = DMDAGetInfo(E->da_b,NULL,&numpts_b,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);CHKERRQ(ierr);
-    ierr = DMDAGetInfo(E->da_s,NULL,&numpts_s,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);CHKERRQ(ierr);
+    ierr = DMDAGetInfo(E->da_b, NULL, &numpts_b, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+    CHKERRQ(ierr);
+    ierr = DMDAGetInfo(E->da_s, NULL, &numpts_s, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+    CHKERRQ(ierr);
 
     /* TODO: dynamically determine number of mesh points, and
        automatically ensure that arr[0] = radius and
        arr[numpts_b-1] = radius * P->coresize */
 
     /* radius at basic nodes */
-    ierr = DMDAGetCorners(da_b,&ilo_b,0,0,&w_b,0,0);CHKERRQ(ierr);
+    ierr = DMDAGetCorners(da_b, &ilo_b, 0, 0, &w_b, 0, 0);
+    CHKERRQ(ierr);
     ihi_b = ilo_b + w_b;
-    ierr = DMDAVecGetArray(da_b,M->xi_b,&arr_b);CHKERRQ(ierr);
-    arr_b[numpts_b-1] =  P->radius * P->coresize;
+    ierr = DMDAVecGetArray(da_b, M->xi_b, &arr_b);
+    CHKERRQ(ierr);
+    arr_b[numpts_b - 1] = P->radius * P->coresize;
     dr_prev = dr_min / geom_fac;
-    for (i=ilo_b; i<ihi_b-1; ++i){ /* Note upper bound */
+    for (i = ilo_b; i < ihi_b - 1; ++i)
+    { /* Note upper bound */
         dr = dr_prev * geom_fac;
-        if( dr > dr_max){
+        if (dr > dr_max)
+        {
             dr = dr_max;
         }
         dr_prev = dr;
-        arr_b[numpts_b-i-2] = arr_b[numpts_b-i-1] + dr;
+        arr_b[numpts_b - i - 2] = arr_b[numpts_b - i - 1] + dr;
     }
     // TODO: below is hacky.
     arr_b[0] = P->radius;
 
-    ierr = DMDAVecRestoreArray(da_b,M->xi_b,&arr_b);CHKERRQ(ierr);
+    ierr = DMDAVecRestoreArray(da_b, M->xi_b, &arr_b);
+    CHKERRQ(ierr);
 
     /* radius at staggered nodes */
-    ierr = DMDAGetCorners(da_s,&ilo_s,0,0,&w_s,0,0);CHKERRQ(ierr);
+    ierr = DMDAGetCorners(da_s, &ilo_s, 0, 0, &w_s, 0, 0);
+    CHKERRQ(ierr);
     ihi_s = ilo_s + w_s;
-    ierr = DMDAVecGetArray(da_s,M->xi_s,&arr_s);CHKERRQ(ierr);
-    for (i=ilo_s;i<ihi_s;++i){
-        arr_s[i] = 0.5 * (arr_b[i]+arr_b[i+1]);
+    ierr = DMDAVecGetArray(da_s, M->xi_s, &arr_s);
+    CHKERRQ(ierr);
+    for (i = ilo_s; i < ihi_s; ++i)
+    {
+        arr_s[i] = 0.5 * (arr_b[i] + arr_b[i + 1]);
     }
-    ierr = DMDAVecRestoreArray(da_s,M->xi_s,&arr_s);CHKERRQ(ierr);
+    ierr = DMDAVecRestoreArray(da_s, M->xi_s, &arr_s);
+    CHKERRQ(ierr);
 
     PetscFunctionReturn(0);
-
 }
 
 /* to merge from Rob's branch */
@@ -202,5 +222,3 @@ static PetscScalar get_layer( DM da, Vec radius, Vec layer, const Parameters P )
     PetscFunctionReturn(0);
 }
 #endif
-
-
